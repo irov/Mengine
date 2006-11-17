482b0043a27f1c01a0e9dfbb14a97bfcc698900d
yuriy_levchenko <yuriy_levchenko@202150c4-1a32-4ab8-af31-e3358ecdd631>
2006-11-17 20:41:56 +0000

    Copied remotely

---
+++ src/System/FileSystem/ZipArch.cpp
@@ -0,0 +1,77 @@
+#	include "ZipArch.h"
+
+#	include "FileData.h"
+
+#	include "zlib\unzip.h"
+#	include "zlib\iowin32.h" 
+
+#	include <assert.h>
+
+ZipArchive::ZipArchive(const std::string& _filename, int _prior) 
+: mUzFile(0),
+mPriority(_prior)
+{
+	assert( _prior > 0 );
+	if(!LoadZip(_filename, _prior))
+	{
+		assert(!"error in FileSystem::ZipArchive()");
+	}
+} 
+
+ZipArchive::~ZipArchive()
+{
+	UnloadZip();
+}
+
+bool ZipArchive::UnloadZip()
+{
+	unzCloseCurrentFile(mUzFile);
+	int nRet = unzClose(mUzFile);
+	mUzFile = NULL;
+	return (nRet == UNZ_OK);
+}
+
+bool ZipArchive::LoadZip(const std::string& _filename, int _prior) 
+{
+	assert( !_filename.empty() );
+
+	mZipName = _filename;
+	mPriority = _prior;
+	UnloadZip();
+	mUzFile = unzOpen( _filename.c_str() );
+	return (mUzFile != NULL);
+}
+
+FileDataInterface* ZipArchive::FileRead(const std::string& _filename) const
+{
+    unz_file_info tagUFI;
+    if(unzLocateFile( mUzFile, _filename.c_str(), 2) == UNZ_OK)
+	{
+        unzGetCurrentFileInfo(mUzFile, &tagUFI, NULL, 0, NULL, 0, NULL, 0);
+		FileDataInterface* retData = new FileData(tagUFI.uncompressed_size);
+		unzOpenCurrentFile(mUzFile);
+        unzReadCurrentFile(mUzFile,(void*)retData->getBuffer(),tagUFI.uncompressed_size);
+        unzCloseCurrentFile(mUzFile);
+        return retData;
+    }
+    return NULL;
+}
+
+bool	ZipArchive::HaveFile(const std::string& _filename) const 
+{
+	if( unzLocateFile(mUzFile, _filename.c_str(), 2) == UNZ_OK)
+	{
+		return true;
+	}
+	return false;
+}
+
+int			ZipArchive::GetPriority() const
+{
+	return mPriority; 
+}
+
+std::string	ZipArchive::GetZipName() const 
+{
+	return mZipName; 
+}

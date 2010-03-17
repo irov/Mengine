a130b3d3a4ba88662a5acc9ed2d1d18f04278788
yuriy_levchenko <yuriy_levchenko@202150c4-1a32-4ab8-af31-e3358ecdd631>
2010-03-17 11:24:24 +0000

    fixed Holder

--- src/Utils/Core/Holder.h
+++ src/Utils/Core/Holder.h
@@ -18,42 +18,27 @@ namespace Menge
 
 		virtual ~Holder()
 		{
-			empty();
+			clear();
 		}
 
-		//! Holder normal method.
-		/*!
-		\sa hostage() destroy()
-		\param c1 the first argument.
-		*/
 		static void keep(T * _hostage)
 		{
 			m_hostage = _hostage;
 		}
 			
-		//! Holder normal method.
-		/*!
-		\sa keep() destroy()
-		\return a pointer object
-		*/
 		static T * hostage()
 		{
 			return m_hostage;
 		}
 
-		//! Holder normal method, destroy keeping object
-		/*!
-		\sa keep() hostage()
-		*/
-		static void destroy()
-		{
-			delete m_hostage;
-			m_hostage = 0;
-		}
-
 		static bool empty()
 		{
 			return m_hostage == 0;
+		}
+
+		static void clear()
+		{
+			m_hostage = 0;
 		}
 
 	private:

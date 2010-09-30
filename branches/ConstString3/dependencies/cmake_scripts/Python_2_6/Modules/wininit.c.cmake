e8da3c73ef7c9d038511ee3823a55377371e13f0
yuriy_levchenko <yuriy_levchenko@202150c4-1a32-4ab8-af31-e3358ecdd631>
2010-09-30 19:50:16 +0000

    1

--- branches/ConstString3/dependencies/cmake_scripts/Python_2_6/Modules/wininit.c.cmake
+++
@@ -1,9 +0,0 @@
-
-#include "windows.h"
-
-char dllVersionBuffer[16] = ""; // a private buffer
-
-// Python Globals
-HMODULE PyWin_DLLhModule = NULL;
-const char *PyWin_DLLVersionString = dllVersionBuffer;
-

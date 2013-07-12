6316e95e43a605aabfdbda05c2482e23af0ca5a3
sniper88 <sniper88@202150c4-1a32-4ab8-af31-e3358ecdd631>
2013-07-12 13:41:24 +0000

    GCC pragma warning fix;

--- trunk/src/System/AstralaxParticleSystem/AstralaxParticleSystem.h
+++ trunk/src/System/AstralaxParticleSystem/AstralaxParticleSystem.h
@@ -2,9 +2,15 @@
 
 #	include "Interface/ParticleSystemInterface.h"
 
-#pragma warning(push, 0) 
+#   ifndef MENGINE_UNSUPPORT_PRAGMA_WARNING
+#	pragma warning(push, 0) 
+#	endif 
+
 #	include "magic.h"
-#pragma warning(pop) 
+
+#   ifndef MENGINE_UNSUPPORT_PRAGMA_WARNING
+#	pragma warning(pop) 
+#	endif
 
 #	include "AstralaxEmitter.h"
 

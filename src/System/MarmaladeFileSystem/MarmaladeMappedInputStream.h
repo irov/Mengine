91846822c0a7f19962a003fcdf88cc0301419e84
sniper88 <sniper88@202150c4-1a32-4ab8-af31-e3358ecdd631>
2014-04-18 14:58:53 +0000

    Update for Marmalade filesystem for deflated zips;

--- src/System/MarmaladeFileSystem/MarmaladeMappedInputStream.h
+++ src/System/MarmaladeFileSystem/MarmaladeMappedInputStream.h
@@ -20,7 +20,7 @@ namespace Menge
 
     public:
         InputStreamInterfacePtr createFileStream() override;
-        bool openFileStream( const InputStreamInterfacePtr & _stream, size_t _offset, size_t _size ) override;
+        bool openFileStream( const InputStreamInterfacePtr & _stream, size_t _offset, size_t _size, void ** _memory ) override;
 
     public:
 		bool initialize( const FilePath & _folder, const FilePath & _fileName ) override;

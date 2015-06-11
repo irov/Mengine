7fb85cfa2d06ae9cbab31c9ee977997f3fcab966
dalexzander <dalexzander@202150c4-1a32-4ab8-af31-e3358ecdd631>
2015-06-11 09:47:37 +0000

    textures binding

--- src/System/MarmaladeRenderSystem/MarmaladeShader.cpp
+++ src/System/MarmaladeRenderSystem/MarmaladeShader.cpp
@@ -25,89 +25,100 @@ namespace Menge
 	{
 	}
     //////////////////////////////////////////////////////////////////////////
-	bool MarmaladeShader::initialize( ServiceProviderInterface * _serviceProvider, const char * _vsSrc, const char * _fsSrc )
-    {
+	bool MarmaladeShader::initialize(ServiceProviderInterface * _serviceProvider, const char * _vsSrc, const char * _fsSrc)
+	{
 		m_serviceProvider = _serviceProvider;
-		        
+
 		GLuint program = glCreateProgram();
 
-		if( program == 0 )
+		if (program == 0)
 		{
 			return false;
 		}
 
-		GLuint vertexShader = this->createShader_( GL_VERTEX_SHADER, _vsSrc );
+		GLuint vertexShader = this->createShader_(GL_VERTEX_SHADER, _vsSrc);
 
-		if( vertexShader == 0 )
+		if (vertexShader == 0)
 		{
 			return false;
 		}
 
-		GLuint fragmentShader = this->createShader_( GL_FRAGMENT_SHADER, _fsSrc );
+		GLuint fragmentShader = this->createShader_(GL_FRAGMENT_SHADER, _fsSrc);
 
-		if( fragmentShader == 0 )
+		if (fragmentShader == 0)
 		{
 			return false;
 		}
 
-		if( (s3eDeviceGetInt( S3E_DEVICE_OS ) != S3E_OS_ID_WS8 &&
-			s3eDeviceGetInt( S3E_DEVICE_OS ) != S3E_OS_ID_WS81 &&
-			s3eDeviceGetInt( S3E_DEVICE_OS ) != S3E_OS_ID_WP81
+		if ((s3eDeviceGetInt(S3E_DEVICE_OS) != S3E_OS_ID_WS8 &&
+			s3eDeviceGetInt(S3E_DEVICE_OS) != S3E_OS_ID_WS81 &&
+			s3eDeviceGetInt(S3E_DEVICE_OS) != S3E_OS_ID_WP81
 			) ||
-			s3eDeviceGetInt( S3E_DEVICE_DX_FEATURE_LEVEL ) < 93
+			s3eDeviceGetInt(S3E_DEVICE_DX_FEATURE_LEVEL) < 93
 			)
 		{
 			// This is needed to compile shaders for Windows Store 8/8.1 and Windows Phone 8.1 using the Win32 Marmalade Simulator.
 			// For more information look at README.IwGLES2.txt.
-			if( IwGetCompileShadersPlatformType() == IW_CS_OS_ID_WS8 ||
+			if (IwGetCompileShadersPlatformType() == IW_CS_OS_ID_WS8 ||
 				IwGetCompileShadersPlatformType() == IW_CS_OS_ID_WS81 ||
 				IwGetCompileShadersPlatformType() == IW_CS_OS_ID_WP81
 				)
 			{
-				s3eRegisterShader( _vsSrc, IW_GL_ST_VERTEX, IW_DX_FL_9_3 );
-				s3eRegisterShader( _fsSrc, IW_GL_ST_PIXEL, IW_DX_FL_9_3 );
+				s3eRegisterShader(_vsSrc, IW_GL_ST_VERTEX, IW_DX_FL_9_3);
+				s3eRegisterShader(_fsSrc, IW_GL_ST_PIXEL, IW_DX_FL_9_3);
 			}
 		}
 
-		glAttachShader( program, vertexShader );
-		glAttachShader( program, fragmentShader );
-		glBindAttribLocation( program, VERTEX_ARRAY, "inVert" );
-		glBindAttribLocation( program, COLOR_ARRAY, "inCol" );
-		glBindAttribLocation( program, UV0_ARRAY, "inUV0" );
-		glBindAttribLocation( program, UV1_ARRAY, "inUV1" );
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+		glBindAttribLocation(program, VERTEX_ARRAY, "inVert");
+		glBindAttribLocation(program, COLOR_ARRAY, "inCol");
+		glBindAttribLocation(program, UV0_ARRAY, "inUV0");
+		glBindAttribLocation(program, UV1_ARRAY, "inUV1");
 
-		glLinkProgram( program );
+		glLinkProgram(program);
 
 		GLint linked;
-		glGetProgramiv( program, GL_LINK_STATUS, &linked );
+		glGetProgramiv(program, GL_LINK_STATUS, &linked);
 
-		if( linked == GL_FALSE )
+		if (linked == GL_FALSE)
 		{
-			LOGGER_ERROR( m_serviceProvider )("RenderMaterialManager::shaderProgram - shader linking error");
+			LOGGER_ERROR(m_serviceProvider)("RenderMaterialManager::shaderProgram - shader linking error");
 
 			return false;
 		}
 
-		glUseProgram( program );
+		glUseProgram(program);
 
-		int transformLocation = glGetUniformLocation( program, "mvpMat" );
+		int transformLocation = glGetUniformLocation(program, "mvpMat");
 
 		m_program = program;
 		m_vertexShader = vertexShader;
 		m_fragmentShader = fragmentShader;
 
 		m_transformLocation = transformLocation;
+		m_samplerLocation[0] = glGetUniformLocation(program, "inSampler0");
+		m_samplerLocation[1] = glGetUniformLocation(program, "inSampler1");
 
 		return true;
     }
 	//////////////////////////////////////////////////////////////////////////
-	void MarmaladeShader::use( const mt::mat4f & _worldMatrix, const mt::mat4f & _viewMatrix, const mt::mat4f & _projectionMatrix ) const
-	{ 	
-		glUseProgram( m_program );
+	void MarmaladeShader::use(const mt::mat4f & _worldMatrix, const mt::mat4f & _viewMatrix, const mt::mat4f & _projectionMatrix) const
+	{
+		glUseProgram(m_program);
 
 		m_mvpMat = _worldMatrix * _viewMatrix * _projectionMatrix;
 
-		glUniformMatrix4fv( m_transformLocation, 1, GL_FALSE, m_mvpMat.buff() );
+		glUniformMatrix4fv(m_transformLocation, 1, GL_FALSE, m_mvpMat.buff());
+	}
+	//////////////////////////////////////////////////////////////////////////
+	void MarmaladeShader::bindTexture(unsigned int _textureInd, int _texture) const
+	{
+		glActiveTexture(GL_TEXTURE0 + _textureInd);
+		glBindTexture(GL_TEXTURE_2D, _texture);
+		if (_textureInd > 1 || m_samplerLocation[_textureInd] < 0)
+			return;
+		glUniform1i(m_samplerLocation[_textureInd], _textureInd);
 	}
 	//////////////////////////////////////////////////////////////////////////
 	void MarmaladeShader::finalize()

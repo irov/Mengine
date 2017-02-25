ac7aef1685366020717326503e2408b1898bbf10
irov <irov13@mail.ru>
2017-02-25 17:28:23 +0200

    fix get dependencies

--- dependencies/GetDepends.bat
+++ dependencies/GetDepends.bat
@@ -10,7 +10,7 @@ if NOT EXIST %toolsdir%\wget\wget.exe goto wget_not_found
 :: extracting 7za.exe
 unzip -o 7za920.zip 7za.exe
 
-call:getdepend http://downloads.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.7z boost_1_62_0.7z boost_1_62_0 boost
+call:getdepend http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.7z boost_1_63_0.7z boost_1_63_0 boost
 
 call:getdepend https://cmake.org/files/v3.7/cmake-3.7.1-win64-x64.zip cmake-3.7.1-win64-x64.zip cmake-3.7.1-win64-x64 cmake
 call:getdepend http://download.sourceforge.net/libpng/lpng1623.zip lpng1623.zip lpng1623 libpng
@@ -34,7 +34,7 @@ call:svndepend https://github.com/irov/pybind.git/trunk pybind
 call:svndepend https://github.com/irov/metabuf.git/trunk metabuf
 call:svndepend https://github.com/irov/stdex.git/trunk stdex
 call:svndepend https://github.com/irov/movie.git/trunk libmovie
-call:svndepend http://svn.code.sf.net/p/utfcpp/code/v2_0 utf8
+call:svndepend https://github.com/nemtrif/utfcpp.git/trunk utf8
 call:svndepend https://github.com/lz4/lz4.git/trunk lz4
 call:svndepend https://github.com/irov/fastpathfinder.git/trunk fastpathfinder
 call:svndependr https://github.com/EsotericSoftware/spine-runtimes.git/trunk/spine-c spine-c 1587
--- dependencies/GetDepends.sh
+++ dependencies/GetDepends.sh
@@ -9,6 +9,18 @@ getdepend()
 	mv $3 $4
 }
 
+getdepend2()
+{
+	if [ ! -f $2 ]; then
+        wget --no-clobber --no-check-certificate $1
+    fi
+
+	rm -rf $5
+	7z x -y $2
+    7z x -y $3
+	mv $4 $5
+}
+
 getdepend_tgz()
 {
 	if [ ! -f $2 ]; then
@@ -25,22 +37,35 @@ svndepend()
     svn checkout -q $1 $2
 }
 
-getdepend http://zlib.net/zlib128.zip ./zlib128.zip zlib-1.2.8 zlib
-getdepend http://download.sourceforge.net/libpng/lpng1616.zip lpng1616.zip lpng1616 libpng
-getdepend http://www.ijg.org/files/jpegsr9a.zip jpegsr9a.zip jpeg-9a libjpeg
-getdepend http://zlib.net/zlib128.zip zlib128.zip zlib-1.2.8 zlib
-getdepend http://downloads.xiph.org/releases/ogg/libogg-1.3.2.zip libogg-1.3.2.zip libogg-1.3.2 libogg
-getdepend http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.zip libvorbis-1.3.4.zip libvorbis-1.3.4 libvorbis
-getdepend http://downloads.xiph.org/releases/theora/libtheora-1.1.1.zip libtheora-1.1.1.zip libtheora-1.1.1 libtheora
-getdepend http://kcat.strangesoft.net/openal-releases/openal-soft-1.16.0.tar.bz2 openal-soft-1.16.0.tar.bz2 openal-soft-1.16.0.tar openal-soft-1.16.0 openal-soft
+svndependr()
+{
+    svn checkout -q -r $3 $1 $2
+}
 
-getdepend_tgz https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz Python-2.7.10.tgz Python-2.7.10 Python
+getdepend http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.7z boost_1_63_0.7z boost_1_63_0 boost
+
+getdepend http://download.sourceforge.net/libpng/lpng1628.zip lpng1628.zip lpng1628 libpng
+getdepend http://www.ijg.org/files/jpegsr9b.zip jpegsr9b.zip jpeg-9b libjpeg
+getdepend http://www.zlib.net/zlib-1.2.11.tar.gz zlib-1.2.11.tar.gz zlib-1.2.11 zlib
+getdepend http://downloads.xiph.org/releases/ogg/libogg-1.3.2.zip libogg-1.3.2.zip libogg-1.3.2 libogg
+getdepend http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.zip libvorbis-1.3.5.zip libvorbis-1.3.5 libvorbis
+getdepend http://downloads.xiph.org/releases/theora/libtheora-1.1.1.zip libtheora-1.1.1.zip libtheora-1.1.1 libtheora
+
+getdepend2 https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.6.0.tar.gz libwebp-0.6.0.tar.gz libwebp-0.6.0.tar libwebp-0.6.0 libwebp
+getdepend2 http://kcat.strangesoft.net/openal-releases/openal-soft-1.17.2.tar.bz2 openal-soft-1.17.2.tar.bz2 openal-soft-1.17.2.tar openal-soft-1.17.2 openal-soft
+
+getdepend_tgz https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz Python-2.7.13.tgz Python-2.7.13 Python
 getdepend_tgz https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz Python-3.4.3.tgz Python-3.4.3 Python3
 
 svndepend http://pugixml.googlecode.com/svn/trunk pugixml
 svndepend https://svn.code.sf.net/p/pybind/code/trunk pybind
 svndepend https://svn.code.sf.net/p/metabuf/code/trunk metabuf
 svndepend https://svn.code.sf.net/p/stdex2/code/trunk stdex
-svndepend http://svn.code.sf.net/p/utfcpp/code/v2_0 utf8
-svndepend http://lz4.googlecode.com/svn/trunk lz4
-svndepend https://svn.code.sf.net/p/fastpathfinder/code fastpathfinder
+svndepend https://github.com/irov/movie.git/trunk libmovie
+svndepend https://github.com/nemtrif/utfcpp.git/trunk utf8
+svndepend https://github.com/lz4/lz4.git/trunk lz4
+svndepend https://github.com/irov/fastpathfinder.git/trunk fastpathfinder
+svndepend https://github.com/erincatto/Box2D.git/trunk/Box2D Box2D
+svndepend https://github.com/irov/GOAP.git/trunk GOAP
+
+svndependr https://github.com/EsotericSoftware/spine-runtimes.git/trunk/spine-c spine-c 1587

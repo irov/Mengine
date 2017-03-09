4e81254286cc2fb1cfaee39cf3b70585aa441acf
irov <irov13@mail.ru>
2017-03-10 01:59:50 +0200

    fix build scripts

--- build/msvc14/make_solution_tools_msvc14_debug.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@pushd ..
-@call cmake_configure "%CD%\..\dependencies\cmake\bin\cmake.exe" "%CD%\..\CMake\Tools_Win32" "..\vs14_solution_tools_debug" "Visual Studio 14" "" "-DCMAKE_CONFIGURATION_TYPES:STRING='Debug'" "-DMENGINE_LIB_DIR:STRING='build_msvc14_debug'" "-DCMAKE_BUILD_TYPE:STRING='Debug'"
-@popd
-
-@pause
-@echo on
--- build/msvc14/make_solution_tools_msvc14_release.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@pushd ..
-@call cmake_configure "%CD%\..\dependencies\cmake\bin\cmake.exe" "%CD%\..\CMake\Tools_Win32" "..\vs14_solution_tools_release" "Visual Studio 14" "" "-DCMAKE_CONFIGURATION_TYPES:STRING='Release'" "-DMENGINE_LIB_DIR:STRING='build_msvc14_release'" "-DCMAKE_BUILD_TYPE:STRING='Release'"
-@popd
-
-@pause
-@echo on

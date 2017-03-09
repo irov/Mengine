4ba954828ae35c72d666d656bb2b008a07bc7ddd
irov <irov13@mail.ru>
2017-03-10 01:53:31 +0200

    add msvc15 build script fix build scripts

--- build/make_solution.bat
+++ build/make_solution.bat
@@ -1,9 +1,4 @@
-@echo off
-
 set "CMAKE_EXE=%CD%\..\dependencies\cmake\bin\cmake.exe"
 set "SOLUTION_DIR=..\solutions\%2"
 
 @call cmake_configure %CMAKE_EXE% %1 %SOLUTION_DIR% %3 "" "-DCMAKE_CONFIGURATION_TYPES:STRING='%4'" "-DCMAKE_BUILD_TYPE:STRING='%4'" "-DMENGINE_LIB_DIR:STRING='%5'"
-
-@pause
-@echo on

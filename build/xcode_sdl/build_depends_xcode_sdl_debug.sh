3da80ffce58a9c673f76286207f8374c29cf321e
irov(mac) <irov13@mail.ru>
2017-05-20 18:38:38 +0300

    xcode

--- build/xcode_sdl/build_depends_xcode_sdl_debug.sh
+++ build/xcode_sdl/build_depends_xcode_sdl_debug.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ../../build_temp/build_xcode_sdl_debug
 pushd ../../build_temp/build_xcode_sdl_debug
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug'
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.9.sdk/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 
 /Applications/CMake.app/Contents/bin/cmake --build ./ --config Debug
 popd
--- build/xcode_sdl/make_solution_xcode_sdl_debug.sh
+++ build/xcode_sdl/make_solution_xcode_sdl_debug.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ../../solutions/solution_xcode_sdl_debug
 pushd ../../solutions/solution_xcode_sdl_debug
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DMENGINE_LIB_DIR:STRING='build_xcode_sdl_debug'
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DMENGINE_LIB_DIR:STRING='build_xcode_sdl_debug' -DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.9.sdk/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 popd

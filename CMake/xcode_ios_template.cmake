8a33808d918ea2f47d643e28c78434816ba0347a
irov(mac) <irov13@mail.ru>
2017-05-23 20:32:49 +0300

    xcode

--- CMake/xcode_ios_template.cmake
+++ CMake/xcode_ios_template.cmake
@@ -1,9 +1,8 @@
-# SET LIBNAME
 SET( MENGINE_LIB_PREFIX "lib" )
 SET( MENGINE_LIB_SUFFIX ".a" )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -arch armv7 ")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch armv7 -std=c++11 -stdlib=libc++ -std=c++1y ")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++ -std=c++1y ")
 
 set(LLVM_ENABLE_CXX1Y ON CACHE BOOL "" FORCE)
 
--- src/System/OpenGLRenderSystem/OpenGLRenderImage.cpp
+++ src/System/OpenGLRenderSystem/OpenGLRenderImage.cpp
@@ -247,11 +247,13 @@ namespace Menge
 			case GL_ETC1_RGB8_OES:
 			case GL_COMPRESSED_RGB_PVRTC_4BPPV1_IMG:
 			case GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG:
+#ifdef _WIN32
 			case GL_COMPRESSED_RGB_S3TC_DXT1_EXT:
+#endif
 				{
 					GLuint textureMemorySize = Helper::getTextureMemorySize( miplevel_hwwidth, miplevel_hwheight, m_hwChannels, 1, m_hwPixelFormat );
 
-					IF_GLCALL( m_serviceProvider, glCompressedTexImage2D_, (GL_TEXTURE_2D, m_lockLevel, m_internalFormat, miplevel_hwwidth, miplevel_hwheight, 0, textureMemorySize, memory) )
+					IF_GLCALL( m_serviceProvider, glCompressedTexImage2D, (GL_TEXTURE_2D, m_lockLevel, m_internalFormat, miplevel_hwwidth, miplevel_hwheight, 0, textureMemorySize, memory) )
 					{
 						LOGGER_ERROR( m_serviceProvider )("OpenGLTexture::unlock glCompressedTexImage2D error\n level %d\n width %d\n height %d\n InternalFormat %d\n PixelFormat %d\n size %d"
 							, _level
@@ -291,7 +293,9 @@ namespace Menge
 			case GL_ETC1_RGB8_OES:
 			case GL_COMPRESSED_RGB_PVRTC_4BPPV1_IMG:
 			case GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG:
+#ifdef _WIN32
 			case GL_COMPRESSED_RGB_S3TC_DXT1_EXT:
+#endif
 				{
 					successful = false;
 				}break;

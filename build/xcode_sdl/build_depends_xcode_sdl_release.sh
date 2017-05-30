8fb9be191e7fbc5319d61bab2c2c6123ebee811b
irov(mac) <irov13@mail.ru>
2017-05-30 20:22:54 +0300

    fix

--- build/xcode_sdl/build_depends_xcode_sdl_release.sh
+++ build/xcode_sdl/build_depends_xcode_sdl_release.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ../../build_temp/build_xcode_sdl_release
 pushd ../../build_temp/build_xcode_sdl_release
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Release' -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.9.sdk/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Release' -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 
 /Applications/CMake.app/Contents/bin/cmake --build ./ --config Release
 popd

ebe24ccc454b32d32433f25cc117da4a65fb8b8e
irov(mac) <irov13@mail.ru>
2017-06-07 20:54:51 +0300

    xcode

--- build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
+++ build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
@@ -2,6 +2,5 @@
 
 mkdir -p ../../build_temp/build_xcode_ios_sdl_debug
 pushd ../../build_temp/build_xcode_ios_sdl_debug
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
-/Applications/CMake.app/Contents/bin/cmake --build ./ --config Debug
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 popd

ac7bab49f1edfbe1339fd4b0799dac123ab83412
irov(mac) <irov13@mail.ru>
2017-06-10 18:36:17 +0300

    no message

--- build/xcode_ios_sdl/make_solution_xcode_ios_sdl_debug.sh
+++ build/xcode_ios_sdl/make_solution_xcode_ios_sdl_debug.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ../../solutions/solution_xcode_ios_sdl_debug
 pushd ../../solutions/solution_xcode_ios_sdl_debug
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DMENGINE_LIB_DIR:STRING='build_xcode_ios_sdl_debug' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DMENGINE_LIB_DIR:STRING='build_xcode_ios_sdl_debug' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -DCMAKE_OSX_ARCHITECTURES="arm64"
 popd

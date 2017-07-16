fa2db78c40a6f690e8ebbf31753b097f9b0c748a
irov(mac) <irov13@mail.ru>
2017-07-16 20:10:41 +0300

    xcode

--- CMake/xcode_ios_template.cmake
+++ CMake/xcode_ios_template.cmake
@@ -1,6 +1,8 @@
 SET( MENGINE_LIB_PREFIX "lib" )
 SET( MENGINE_LIB_SUFFIX ".a" )
 
+set(CMAKE_DEBUG_POSTFIX "" CACHE STRING "Set debug library postfix" FORCE)
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++ -std=c++1y ")
 
--- build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
+++ build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ../../solutions/solution_xcode_ios_sdl_release
 pushd ../../solutions/solution_xcode_ios_sdl_release
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Release' -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DMENGINE_LIB_DIR:STRING='build_xcode_ios_sdl_release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -DCMAKE_OSX_ARCHITECTURES="armv7, arm64"
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING='Release' -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DMENGINE_LIB_DIR:STRING='build_xcode_ios_sdl_release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -DCMAKE_OSX_ARCHITECTURES=${ARCHS_STANDARD}
 popd

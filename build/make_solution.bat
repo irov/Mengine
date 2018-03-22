9799bb07949ebfcb2c2731567abae2ef50ec584e
irov <irov13@mail.ru>
2018-03-22 14:03:02 +0200

    some fix

--- build/make_solution.bat
+++ build/make_solution.bat
@@ -1,4 +1,3 @@
-set "CMAKE_EXE=%CD%\..\dependencies\cmake\bin\cmake.exe"
 set "SOLUTION_DIR=..\solutions\%2"
 
-@call cmake_configure.bat %CMAKE_EXE% %1 %SOLUTION_DIR% %3 "" "-DCMAKE_CONFIGURATION_TYPES:STRING='%4'" "-DCMAKE_BUILD_TYPE:STRING='%4'" "-DMENGINE_LIB_DIR:STRING='%5'"
+@call cmake_configure.bat CMake %1 %SOLUTION_DIR% %3 "" "-DCMAKE_CONFIGURATION_TYPES:STRING='%4'" "-DCMAKE_BUILD_TYPE:STRING='%4'" "-DMENGINE_LIB_DIR:STRING='%5'"

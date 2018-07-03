a6c3e275672a01dcf10968a33a28d4f1b70c98de
Alex Korzh <alexkorzh7@gmail.com>
2018-07-03 23:29:40 +0300

    Fix naming, add method call

--- gradle/app/src/org/Mengine/Build/UnityAds/UnityAdsInteractionLayer.java
+++ gradle/app/src/org/Mengine/Build/UnityAds/UnityAdsInteractionLayer.java
@@ -17,17 +17,17 @@ public class UnityAdsInteractionLayer implements IUnityAdsExtendedListener {
 
     final private String _gameId = "2654327";
 
-    static native void AndroidNativeUnity_onUnityAdsClick(String s);
+    static native void AndroidNativeUnity_onUnityAdsClick(String placementId);
 
-    static native void AndroidNativeUnity_onUnityAdsPlacementStateChanged(String s, int placementState, int placementState1);
+    static native void AndroidNativeUnity_onUnityAdsPlacementStateChanged(String placementId, int placementState, int placementState1);
 
-    static native void AndroidNativeUnity_onUnityAdsReady(String s);
+    static native void AndroidNativeUnity_onUnityAdsReady(String placementId);
 
-    static native void AndroidNativeUnity_onUnityAdsStart(String s);
+    static native void AndroidNativeUnity_onUnityAdsStart(String placementId);
 
-    static native void AndroidNativeUnity_onUnityAdsFinish(String s, int finishState);
+    static native void AndroidNativeUnity_onUnityAdsFinish(String placementId, int finishState);
 
-    static native void AndroidNativeFacebook_onUnityAdsError(int unityAdsError, String s);
+    static native void AndroidNativeUnity_onUnityAdsError(int unityAdsError, String message);
 
     public UnityAdsInteractionLayer() {
         if (Build.VERSION.SDK_INT >= 19) {
@@ -47,31 +47,31 @@ public class UnityAdsInteractionLayer implements IUnityAdsExtendedListener {
 
     @Override
     public void onUnityAdsClick(String placementId) {
-
+        AndroidNativeUnity_onUnityAdsClick(placementId);
     }
 
     @Override
     public void onUnityAdsPlacementStateChanged(String placementId, UnityAds.PlacementState placementState, UnityAds.PlacementState placementState1) {
-
+        AndroidNativeUnity_onUnityAdsPlacementStateChanged(placementId, placementState.ordinal(), placementState1.ordinal());
     }
 
     @Override
     public void onUnityAdsReady(String placementId) {
-
+        AndroidNativeUnity_onUnityAdsReady(placementId);
     }
 
     @Override
     public void onUnityAdsStart(String placementId) {
-
+        AndroidNativeUnity_onUnityAdsStart(placementId);
     }
 
     @Override
     public void onUnityAdsFinish(String placementId, UnityAds.FinishState finishState) {
-
+        AndroidNativeUnity_onUnityAdsFinish(placementId, finishState.ordinal());
     }
 
     @Override
     public void onUnityAdsError(UnityAds.UnityAdsError unityAdsError, String message) {
-
+        AndroidNativeUnity_onUnityAdsError(unityAdsError.ordinal(), message);
     }
 }

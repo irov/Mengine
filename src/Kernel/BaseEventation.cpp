1062e9ac078475e684bda06171094364f9533cdd
yuriy.levchenko <irov13@mail.ru>
2018-12-12 21:41:54 +0200

    fix

--- src/Kernel/BaseEventation.cpp
+++ src/Kernel/BaseEventation.cpp
@@ -79,7 +79,7 @@ namespace Mengine
 
         m_receivers.erase( it_found );
 
-        m_flag &= ~(1L << _event);
+        m_flag &= ~(1U << _event);
     }
     //////////////////////////////////////////////////////////////////////////
     const EventReceiverInterfacePtr & BaseEventation::getEventReciever( uint32_t _event ) const

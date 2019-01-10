dcf9fd6f9eaf30275f934e252a91d4c6ee19fdbe
yuriy.levchenko <irov13@mail.ru>
2019-01-10 20:36:41 +0200

    wip fix ios

--- build/xcode_sdl/make_solution_xcode_sdl_release.sh
+++ build/xcode_sdl/make_solution_xcode_sdl_release.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-mkdir -p ../../solutions/solution_xcode_sdl_release
-pushd ../../solutions/solution_xcode_sdl_release
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='Release' -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DMENGINE_LIB_DIR:STRING='build_xcode_sdl_release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
+CONFIGURATION=Release
+
+mkdir -p ../../solutions/solution_xcode_sdl/$CONFIGURATION
+pushd ../../solutions/solution_xcode_sdl/$CONFIGURATION
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='$CONFIGURATION' -DCMAKE_CONFIGURATION_TYPES:STRING='$CONFIGURATION' -DMENGINE_LIB_DIR:STRING='build_xcode_sdl_release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 popd

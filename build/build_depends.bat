2c1795879f5b0f3dadb0c4184e2486c7900cf88b
irov <irov13@mail.ru>
2019-03-05 13:31:15 +0200

    try remove high log for travis

--- build/build_depends.bat
+++ build/build_depends.bat
@@ -3,5 +3,5 @@ set "BUILD_TEMP_DIR=..\build_temp\%2"
 @call cmake_configure.bat CMake %1 %BUILD_TEMP_DIR% %3 "" "-DCMAKE_CONFIGURATION_TYPES:STRING='%4'" "-DCMAKE_BUILD_TYPE:STRING='%4'"
 
 @pushd %BUILD_TEMP_DIR%
-CMake --build .\ --config %4 
+CMake --build .\ --config %4 -- /verbosity:minimal
 @popd
--- build/downloads/downloads.bat
+++ build/downloads/downloads.bat
@@ -4,11 +4,11 @@
 
 @mkdir %~dp0..\..\build_temp\downloads
 @pushd %~dp0..\..\build_temp\downloads
-@call CMake.exe "%CD%\..\..\CMake\Downloads"
+@call CMake.exe "%CD%\..\..\CMake\Downloads" -DMENGINE_DOWNLOADS_SILENT=ON
 @popd
 
 @pushd %~dp0..\..\build_temp\downloads
-@call CMake.exe --build .
+@call CMake.exe --build . -- /verbosity:minimal
 @popd
 
 @echo Done

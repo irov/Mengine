38c09f0f8cb585bc8106df1e8337e6b1742c3401
irov(mac) <irov13@mail.ru>
2019-03-11 21:09:31 +0200

    mac, some fix

--- build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
+++ build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Release
 
 mkdir -p ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DMENGINE_LIB_DIR:STRING="build_xcode_ios_sdl/\"$CONFIGURATION\"" -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DIOS_PLATFORM=OS64 -DENABLE_BITCODE=FALSE
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DMENGINE_LIB_DIR:STRING="build_xcode_ios_sdl/\"$CONFIGURATION\"" -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DIOS_PLATFORM=OS64 -DENABLE_BITCODE=FALSE -DIOS_DEPLOYMENT_TARGET="9.0"
 popd

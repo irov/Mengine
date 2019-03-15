90fd53cc7fd7d74d540a3e5a521a75e4a58e71c9
yuriy.levchenko <irov13@mail.ru>
2019-03-15 17:37:17 +0200

    fix SDL build

--- src/Plugins/SDLFileGroupPlugin/SDLFileGroupPlugin.cpp
+++ src/Plugins/SDLFileGroupPlugin/SDLFileGroupPlugin.cpp
@@ -1,9 +1,9 @@
 #include "SDLFileGroupPlugin.h"
 
 #include "Interface/StringizeServiceInterface.h"
-#include "Interface/FileServiceInterface.h"
 #include "Interface/PlatformInterface.h"
 #include "Interface/UnicodeSystemInterface.h"
+#include "Interface/VocabularyServiceInterface.h"
 
 #include "SDLFileGroupDirectory.h"
 #include "SDLFileGroupDirectoryFactory.h"
@@ -26,10 +26,7 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     bool SDLFileGroupPlugin::_initializePlugin()
     {
-        FILE_SERVICE()
-            ->registerFileGroupFactory( STRINGIZE_STRING_LOCAL( "global" )
-                , Helper::makeFactory<SDLFileGroupDirectoryFactory>( STRINGIZE_FILEPATH_LOCAL( "" ) )
-            );
+        VOCABULARY_SET( Factory, STRINGIZE_STRING_LOCAL( "FileGroupFactory" ), STRINGIZE_STRING_LOCAL( "global" ), Helper::makeFactory<SDLFileGroupDirectoryFactory>( STRINGIZE_FILEPATH_LOCAL( "" ) ) );
 
         Char utf8_currentPath[MENGINE_MAX_PATH];
         size_t utf8_currentPathLen = PLATFORM_SERVICE()
@@ -37,20 +34,14 @@ namespace Mengine
 
         FilePath relationPath = Helper::stringizeFilePathSize( utf8_currentPath, utf8_currentPathLen );
 
-        FILE_SERVICE()
-            ->registerFileGroupFactory( STRINGIZE_STRING_LOCAL( "dir" )
-                , Helper::makeFactory<SDLFileGroupDirectoryFactory>( relationPath )
-            );
+        VOCABULARY_SET( Factory, STRINGIZE_STRING_LOCAL( "FileGroupFactory" ), STRINGIZE_STRING_LOCAL( "dir" ), Helper::makeFactory<SDLFileGroupDirectoryFactory>( relationPath ) );
 
         return true;
     }
     //////////////////////////////////////////////////////////////////////////
     void SDLFileGroupPlugin::_finalizePlugin()
     {
-        FILE_SERVICE()
-            ->unregisterFileGroupFactory( STRINGIZE_STRING_LOCAL( "global" ) );
-
-        FILE_SERVICE()
-            ->unregisterFileGroupFactory( STRINGIZE_STRING_LOCAL( "dir") );
+        VOCABULARY_REMOVE( STRINGIZE_STRING_LOCAL( "FileGroupFactory" ), STRINGIZE_STRING_LOCAL( "global" ) );
+        VOCABULARY_REMOVE( STRINGIZE_STRING_LOCAL( "FileGroupFactory" ), STRINGIZE_STRING_LOCAL( "dir" ) );
     }
 }

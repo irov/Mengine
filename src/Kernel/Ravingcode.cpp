4dff4c63fc187d51c3bbae096d38ea42372dc821
yuriy.levchenko <irov13@mail.ru>
2019-05-06 20:17:04 +0300

    add Ravingcode

---
+++ src/Kernel/Ravingcode.cpp
@@ -0,0 +1,25 @@
+#include "Ravingcode.h"
+
+namespace Mengine
+{
+    namespace Helper
+    {
+        void ravingcode( uint32_t _seed, const void * _in, size_t _size, void * _out )
+        {
+            const uint8_t * u8_in = (const uint8_t *)_in;
+            uint8_t * u8_out = (uint8_t *)_out;
+
+            uint32_t seed = _seed;
+            while( _size-- )
+            {
+                seed *= 1103515245U;
+                seed += 12345U;
+                seed ^= seed << 13;
+                seed ^= seed >> 17;
+                seed ^= seed << 5;
+
+                *(u8_out++) = (uint8_t)(*(u8_in++) ^ seed);
+            }
+        }
+    }
+}

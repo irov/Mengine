3eb0be3bdf3c3e8fa16f2a72549025724014f6a1
yuriy.levchenko <irov13@mail.ru>
2019-05-06 20:17:04 +0300

    add Ravingcode

---
+++ src/Kernel/Ravingcode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Config/Typedef.h"
+
+namespace Mengine
+{
+    namespace Helper
+    {
+        void ravingcode( uint32_t _seed, const void * _in, size_t _size, void * _out );
+    }
+}

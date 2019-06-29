d4c338477f84278e33205d70303a6e53b35febab
yuriy.levchenko <irov13@mail.ru>
2019-06-29 21:09:19 +0300

    add wip examples

---
+++ examples/example_demo/Personality.py
@@ -0,0 +1,16 @@
+from Main import Main
+
+def onPreparation(isDebug):
+    Mengine.setCursorMode(True)
+
+    return True
+    pass
+
+def onRun():
+    Scene = Mengine.createScene("Main", Main)
+
+    Mengine.setCurrentScene(Scene, "Test", False, True, None)
+    pass
+
+def onFinalize():
+    pass

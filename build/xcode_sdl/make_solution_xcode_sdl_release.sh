a83a1a76536aa2969948dff9b6461e9d64870eec
irov <irov13@mail.ru>
2019-07-29 19:41:00 +0300

    some build fix

--- build/xcode_sdl/make_solution_xcode_sdl_release.sh
+++ build/xcode_sdl/make_solution_xcode_sdl_release.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Release
 
 mkdir -p ../../solutions/solution_xcode_sdl/$CONFIGURATION
 pushd ../../solutions/solution_xcode_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='$CONFIGURATION' -DCMAKE_CONFIGURATION_TYPES:STRING='$CONFIGURATION' -DMENGINE_LIB_DIR:STRING='build_xcode_sdl_release' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='$CONFIGURATION' -DCMAKE_CONFIGURATION_TYPES:STRING='$CONFIGURATION' -DMENGINE_LIB_DIR:STRING='Depends_Xcode_SDL' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 popd

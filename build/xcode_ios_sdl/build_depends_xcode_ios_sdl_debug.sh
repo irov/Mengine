85b3f9486839d7a3ae287278880f578a65f1c0f8
yuriy.levchenko <irov13@mail.ru>
2019-07-29 21:39:13 +0300

    fix ios build

--- build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
+++ build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Debug
 
 mkdir -p ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DIOS_PLATFORM=OS64 -DENABLE_BITCODE=0 -DIOS_DEPLOYMENT_TARGET="9.0"
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../CMake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DPLATFORM=OS64 -DENABLE_BITCODE=0 -DDEPLOYMENT_TARGET="9.0"
 popd

19fa8888ea516bd5de05e40cf20bc6baa4b44746
Aleksei Korzh <a.korzh@rocketguys.com>
2019-08-07 21:15:51 +0300

    Add notif. channels and do minor fixes and refactoring

--- gradle/app/src/org/Mengine/Build/LocalNotifications/LocalNotificationsInteractionLayer.java
+++ gradle/app/src/org/Mengine/Build/LocalNotifications/LocalNotificationsInteractionLayer.java
@@ -2,20 +2,37 @@ package org.Mengine.Build.LocalNotifications;
 
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.SystemClock;
-import android.support.v4.app.NotificationManagerCompat;
 
 import org.Mengine.Build.MengineActivity;
 import org.Mengine.Build.R;
 
 public class LocalNotificationsInteractionLayer {
     private static Context _currentContext;
+    private static final String channelId = "mengine_channel_id";
+    private static final CharSequence channelName = "Mengine Channel";
 
     public LocalNotificationsInteractionLayer(Context context) {
         _currentContext = context;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{1000, 2000});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 
     public void scheduleNotification(Notification notification, int id, int delay) {
@@ -31,7 +48,7 @@ public class LocalNotificationsInteractionLayer {
     }
 
     public void instantlyPresentNotification(Notification notification, int id) {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(_currentContext);
+        NotificationManager notificationManager = (NotificationManager)_currentContext.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(id, notification);
     }
 
@@ -45,7 +62,12 @@ public class LocalNotificationsInteractionLayer {
         builder.setContentTitle(title);
         builder.setContentText(content);
         builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setDefaults(Notification.DEFAULT_ALL);
         builder.setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(channelId);
+        }
+        builder.setPriority(Notification.PRIORITY_MAX);
         return builder.build();
     }
 }

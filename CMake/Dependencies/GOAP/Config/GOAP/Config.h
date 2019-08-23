a0a9e826276890c972697afa80aa41e5e4a2666f
irov <irov13@mail.ru>
2019-08-23 13:50:32 +0300

    update GOAP

---
+++ CMake/Dependencies/GOAP/Config/GOAP/Config.h
@@ -0,0 +1,18 @@
+/*
+* Copyright (C) 2017-2019, Yuriy Levchenko <irov13@mail.ru>
+*
+* This software may be modified and distributed under the terms
+* of the MIT license.  See the LICENSE file for details.
+*/
+
+#pragma once
+
+#include <stdint.h>
+#include <cstddef>
+
+#include "stdex/allocator.h"
+
+#define GOAP_UNUSED(VALUE) ((void)VALUE)
+
+#define GOAP_MALLOC(SIZE) (stdex_malloc(SIZE, "GOAP"))
+#define GOAP_FREE(PTR, SIZE) (stdex_free(PTR, "GOAP"))

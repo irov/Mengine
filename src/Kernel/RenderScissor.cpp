213982014d551b97e12804d7a7662b1f875b73eb
irov <irov13@mail.ru>
2019-09-11 18:50:30 +0300

    add CMake MENGINE_INSTALL_PATH improve RenderScissor fix PlayerService

--- src/Kernel/RenderScissor.cpp
+++ src/Kernel/RenderScissor.cpp
@@ -1,5 +1,8 @@
 #include "RenderScissor.h"
 
+#include "Interface/ApplicationInterface.h"
+#include "Interface/NotificationServiceInterface.h"
+
 namespace Mengine
 {
     //////////////////////////////////////////////////////////////////////////
@@ -19,9 +22,18 @@ namespace Mengine
             return true;
         }
 
+        NOTIFICATION_ADDOBSERVERMETHOD( NOTIFICATOR_CHANGE_WINDOW_RESOLUTION, this, &RenderScissor::notifyChangeWindowResolution );
+
         this->invalidateViewport_();
 
         return true;
+    }
+    //////////////////////////////////////////////////////////////////////////
+    void RenderScissor::_deactivate()
+    {
+        Node::_deactivate();
+
+        NOTIFICATION_REMOVEOBSERVER_THIS( NOTIFICATOR_CHANGE_WINDOW_RESOLUTION );
     }
     //////////////////////////////////////////////////////////////////////////
     void RenderScissor::setViewport( const Viewport & _viewport )
@@ -47,9 +59,47 @@ namespace Mengine
     {
         m_invalidateViewport = false;
 
+        float gameViewportAspect; 
+        Viewport gameViewport;
+
+        const Resolution & contentResolution = APPLICATION_SERVICE()
+            ->getContentResolution();
+
+        APPLICATION_SERVICE()
+            ->getGameViewport( gameViewportAspect, gameViewport );
+
+        mt::vec2f contentResolutionSize;
+        contentResolution.calcSize( contentResolutionSize );
+
+        mt::vec2f gameViewportSize;
+        gameViewport.calcSize( gameViewportSize );
+
+        mt::vec2f viewportMaskBegin = gameViewport.begin / contentResolutionSize;
+        mt::vec2f viewportMaskEnd = gameViewport.end / contentResolutionSize;
+
+        mt::vec2f viewportMaskSize = viewportMaskEnd - viewportMaskBegin;
+
         const mt::mat4f & wm = this->getWorldMatrix();
 
-        mt::mul_v2_v2_m4( m_viewportWM.begin, m_viewport.begin, wm );
-        mt::mul_v2_v2_m4( m_viewportWM.end, m_viewport.end, wm );
+        Viewport viewportWM;
+        mt::mul_v2_v2_m4( viewportWM.begin, m_viewport.begin, wm );
+        mt::mul_v2_v2_m4( viewportWM.end, m_viewport.end, wm );
+
+        viewportWM.begin /= contentResolutionSize;
+        viewportWM.end /= contentResolutionSize;
+
+        m_viewportWM.begin = (viewportWM.begin - viewportMaskBegin) / viewportMaskSize * contentResolutionSize;
+        m_viewportWM.end = (viewportWM.end - viewportMaskBegin) / viewportMaskSize * contentResolutionSize;
+
+        Viewport contentResolutionViewport( mt::vec2f( 0.f, 0.f ), contentResolutionSize );
+        m_viewportWM.clamp( contentResolutionViewport );
+    }
+    //////////////////////////////////////////////////////////////////////////
+    void RenderScissor::notifyChangeWindowResolution( bool _fullscreen, const Resolution & _resolution )
+    {
+        MENGINE_UNUSED( _fullscreen );
+        MENGINE_UNUSED( _resolution );
+
+        this->invalidateViewport_();
     }
 }

d78919d01fcb0c21e92f90baf88fd4c5e333fe7a
irov <irov13@mail.ru>
2019-10-10 16:04:40 +0300

    add DateTimeProviderInterface getTimeZoneOffset

--- src/Platforms/Win32Platform/Win32DateTimeProvider.h
+++ src/Platforms/Win32Platform/Win32DateTimeProvider.h
@@ -12,6 +12,9 @@ namespace Mengine
 
     protected:
         uint64_t getLocalDateMilliseconds() const override;
+
+    protected:
+        int32_t getTimeZoneOffset() const override;
     };
     //////////////////////////////////////////////////////////////////////////
     typedef IntrusivePtr<Win32DateTimeProvider, DateTimeProviderInterface> Win32DateTimeProviderPtr;

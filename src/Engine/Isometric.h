a2e75ea5689a0a64d09ab23d83a05bcb5c65edeb
irov <irov13@mail.ru>
2019-10-24 15:24:49 +0300

    improve NodeGenerators

---
+++ src/Engine/Isometric.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Kernel/Node.h"
+#include "Kernel/BaseUpdation.h"
+
+namespace Mengine
+{
+    class Isometric
+        : public Node
+        , public BaseUpdation
+    {
+        DECLARE_VISITABLE( Node );
+        DECLARE_UPDATABLE();
+
+    public:
+        Isometric();
+        ~Isometric() override;
+
+    protected:
+        void update( const UpdateContext * _context ) override;
+    };
+}

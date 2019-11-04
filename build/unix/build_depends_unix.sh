c0f136da21cdff88c00b19d874d517a3d87280a1
irov <irov13@mail.ru>
2019-11-04 20:13:08 +0200

    fix

--- build/unix/build_depends_unix.sh
+++ build/unix/build_depends_unix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIGURATION=$1
-
+echo $CONFIGURATION
 mkdir -p ../../solutions/solution_unix/$CONFIGURATION
 pushd ../../solutions/solution_unix/$CONFIGURATION
 cmake -G "Unix Makefiles" -S "../../../cmake/Depends_Unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION

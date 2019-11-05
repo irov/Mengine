1ceb6ee44061c5c7fec550021d5c3e293877cc4b
irov <irov13@mail.ru>
2019-11-05 14:03:43 +0200

    wip add solution Unix

--- build/unix/build_depends_unix.sh
+++ build/unix/build_depends_unix.sh
@@ -2,8 +2,8 @@
 
 CONFIGURATION=$1
 
-mkdir -p ../../solutions/solution_unix/$CONFIGURATION
-pushd ../../solutions/solution_unix/$CONFIGURATION
+mkdir -p ../../solutions/dependencies_unix/$CONFIGURATION
+pushd ../../solutions/dependencies_unix/$CONFIGURATION
 cmake -G "Unix Makefiles" -S "../../../cmake/Depends_Unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
 cmake --build ./ --config $CONFIGURATION
 popd
---
+++ build/unix/build_solution_unix.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONFIGURATION=$1
+
+mkdir -p ../../solutions/solution_unix/$CONFIGURATION
+pushd ../../solutions/solution_unix/$CONFIGURATION
+cmake -G "Unix Makefiles" -S "../../../cmake/Unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
+cmake --build ./ --config $CONFIGURATION
+popd

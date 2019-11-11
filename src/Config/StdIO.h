5ba5a6ebc62c5739576b0afba1a8cdd9b8ddf6c9
irov <irov13@mail.ru>
2019-11-11 13:09:31 +0200

    add StdIO MENGINE_STRICMP fix FactoryService mutex

--- src/Config/StdIO.h
+++ src/Config/StdIO.h
@@ -8,7 +8,13 @@
 #define MENGINE_VA_LIST_END( Args ) va_end( Args )
 
 #ifdef MENGINE_TOOLCHAIN_MSVC
-#define MENGINE_VSNPRINTF(Buffer, Capacity, Format, Args) ::vsnprintf( Buffer, Capacity, Format, Args )
+    #define MENGINE_VSNPRINTF(Buffer, Capacity, Format, Args) ::vsnprintf( Buffer, Capacity, Format, Args )
 #else
-#define MENGINE_VSNPRINTF(Buffer, Capacity, Format, Args) ::vsprintf( Buffer, Format, Args )
+    #define MENGINE_VSNPRINTF(Buffer, Capacity, Format, Args) ::vsprintf( Buffer, Format, Args )
+#endif
+
+#ifdef MENGINE_TOOLCHAIN_MSVC
+    #define MENGINE_STRICMP _stricmp
+#else
+    #define MENGINE_STRICMP stricmp
 #endif

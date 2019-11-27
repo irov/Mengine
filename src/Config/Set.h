f1c9776e3b6d379852d0519947d53b1594e5a8e4
irov <irov13@mail.ru>
2019-11-27 16:06:39 +0200

    fix Set

--- src/Config/Set.h
+++ src/Config/Set.h
@@ -6,6 +6,6 @@
 
 namespace Mengine
 {
-    template<class K, class L = std::less<K>, class A = stdex::stl_allocator<std::pair<const K, T>>>
+    template<class K, class L = std::less<K>, class A = stdex::stl_allocator<K>>>
     using Set = std::set<K, L, A>;
 }

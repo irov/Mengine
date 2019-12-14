c024a8c951034d93c5fe35a78f8d703afcae7c32
yuriy.levchenko <irov13@mail.ru>
2019-12-14 19:23:53 +0200

    fix xcode

--- build/xcode_sdl/make_solution_xcode_sdl_debug.sh
+++ build/xcode_sdl/make_solution_xcode_sdl_debug.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Debug
 
 mkdir -p ../../solutions/solution_xcode_sdl/$CONFIGURATION
 pushd ../../solutions/solution_xcode_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING='$CONFIGURATION' -DCMAKE_CONFIGURATION_TYPES:STRING='$CONFIGURATION' -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 popd

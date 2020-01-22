4a84a1aa8a74e6721e761fb0382e75315954bdf2
irov <irov13@mail.ru>
2020-01-22 16:33:41 +0200

    update Box2D clear code Vectorizator improve AssertionNotImplemented

--- src/Kernel/AssertionNotImplemented.h
+++ src/Kernel/AssertionNotImplemented.h
@@ -3,7 +3,7 @@
 #include "Kernel/Assertion.h"
 
 #ifdef MENGINE_ASSERTION_DEBUG
-#   define MENGINE_ASSERTION_NOT_IMPLEMENTED() Mengine::Helper::Assertion( ASSERTION_LEVEL_WARNING, "true", MENGINE_CODE_FUNCTION, MENGINE_CODE_LINE, "This method not implemented, don't use that" )
+#   define MENGINE_ASSERTION_NOT_IMPLEMENTED() Mengine::Helper::Assertion( ASSERTION_LEVEL_FATAL, "true", MENGINE_CODE_FUNCTION, MENGINE_CODE_LINE, "This method not implemented, don't use that" )
 #else
 #   define MENGINE_ASSERTION_NOT_IMPLEMENTED()
 #endif

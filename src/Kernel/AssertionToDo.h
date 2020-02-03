758ad0389804fcd9fbdaf7848b80a2a089cd8c7a
irov <irov13@mail.ru>
2020-02-03 19:19:49 +0200

    add AssertionToDo

---
+++ src/Kernel/AssertionToDo.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Kernel/Assertion.h"
+
+#ifdef MENGINE_ASSERTION_DEBUG
+#   define MENGINE_ASSERTION_TODO() Mengine::Helper::Assertion( ASSERTION_LEVEL_WARNING, "true", MENGINE_CODE_FUNCTION, MENGINE_CODE_LINE, "This method not implemented, todo.." )
+#else
+#   define MENGINE_ASSERTION_TODO()
+#endif

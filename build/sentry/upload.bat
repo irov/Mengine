790c5af8ffb79183ed0c3264819ba7e6b6c6d44e
irov <irov13@mail.ru>
2020-02-25 15:31:59 +0200

    some VideoPlugin fix

--- build/sentry/upload.bat
+++
@@ -1,5 +0,0 @@
-set SENTRY_ORG=wonderland-9p
-set SENTRY_PROJECT=mengine
-set SENTRY_AUTH_TOKEN=%1
-set SENTRY_PDB_PATH="d:\Projects\landbase_code\Mengine\pdb\Mengine_WIN32\Visual Studio 16 2019\Debug"
-sentry-cli.exe --log-level=debug upload-dif --org %SENTRY_ORG% --project %SENTRY_PROJECT% %SENTRY_AUTH_TOKEN% %SENTRY_PDB_PATH%

cbc0ef73112eb73f85ad5794d4f0d6895a59fa9c
irov(mac) <irov13@mail.ru>
2020-03-10 21:22:24 +0200

    mac

--- build/xcode_ios_sdl/build_depends_xcode_ios_sdl_release.sh
+++ build/xcode_ios_sdl/build_depends_xcode_ios_sdl_release.sh
@@ -5,4 +5,6 @@ CONFIGURATION=Release
 mkdir -p ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
 /Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DPLATFORM=OS64 -DENABLE_BITCODE=0 -DDEPLOYMENT_TARGET="9.0"
+
+/Applications/CMake.app/Contents/bin/cmake --build ./ --config $CONFIGURATION
 popd

7173183ec9b64546ee20547f36ec423cb142b133
yuriy.levchenko <irov13@mail.ru>
2020-03-10 21:21:54 +0200

    test ios

--- build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
+++ build/xcode_ios_sdl/make_solution_xcode_ios_sdl_release.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Release
 
 mkdir -p ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DPLATFORM=OS64 -DENABLE_BITCODE=0 -DDEPLOYMENT_TARGET="9.0"
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/ -DCMAKE_TOOLCHAIN_FILE="$PWD/../../../dependencies/ios-cmake/ios.toolchain.cmake" -DPLATFORM=OS64 -DENABLE_BITCODE=0 -DDEPLOYMENT_TARGET="9.0"
 popd

f93c41a525140d017aeead0fc23ef5013ef20683
yuriy.levchenko <irov13@mail.ru>
2020-03-15 19:43:05 +0200

    fix xcode ios build

--- build/xcode_ios_sdl/make_solution_xcode_ios_sdl_debug.sh
+++ build/xcode_ios_sdl/make_solution_xcode_ios_sdl_debug.sh
@@ -4,5 +4,5 @@ CONFIGURATION=Debug
 
 mkdir -p ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/solution_xcode_ios_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
 popd

21738a1af4277a4786d214c5f45883ebf19a695c
yuriy.levchenko <irov13@mail.ru>
2020-04-06 21:39:18 +0300

    fix ios build

--- build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
+++ build/xcode_ios_sdl/build_depends_xcode_ios_sdl_debug.sh
@@ -5,4 +5,5 @@ CONFIGURATION=Debug
 mkdir -p ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
 pushd ../../solutions/dependencies_xcode_ios_sdl/$CONFIGURATION
 /Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Depends_Xcode_IOS_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
+/Applications/CMake.app/Contents/bin/cmake --build ./ --config $CONFIGURATION
 popd
--- build/xcode_sdl/build_depends_xcode_sdl_debug.sh
+++ build/xcode_sdl/build_depends_xcode_sdl_debug.sh
@@ -4,6 +4,6 @@ CONFIGURATION=Debug
 
 mkdir -p ../../solutions/dependencies_xcode_sdl/$CONFIGURATION
 pushd ../../solutions/dependencies_xcode_sdl/$CONFIGURATION
-/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
+/Applications/CMake.app/Contents/bin/cmake -G"Xcode" "$PWD/../../../cmake/Depends_Xcode_SDL" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9
 /Applications/CMake.app/Contents/bin/cmake --build ./ --config $CONFIGURATION
 popd

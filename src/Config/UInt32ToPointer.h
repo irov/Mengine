3a919e253942b067ded09f440db60c436c3d28c7
Yuriy Levchenko <irov13@mail.ru>
2020-04-15 01:14:43 +0300

    Update UInt32ToPointer.h

--- src/Config/UInt32ToPointer.h
+++ src/Config/UInt32ToPointer.h
@@ -3,7 +3,7 @@
 #include "Config/Config.h"
 
 #ifndef MENGINE_UINT32_TO_POINTER
-#if defined(MENGINE_ARCHITECTURE_X32)
+#if defined(MENGINE_ARCHITECTURE_X86)
 #   define MENGINE_UINT32_TO_POINTER(T, O) ((T)(const uint32_t *)(uint32_t)(O))
 #endif
 

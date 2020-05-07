43acc732535a81f5254a9ede04145430f304808c
irov(mac) <irov13@mail.ru>
2020-05-07 19:44:25 +0300

    fix mac build

--- build/downloads/downloads_macos.sh
+++ build/downloads/downloads_macos.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 if test -f "/usr/local/bin/cmake"; then
-    CMAKE = /usr/local/bin/cmake
+    CMAKE=/usr/local/bin/cmake
 fi
 
 if test -f "/Applications/CMake.app/Contents/bin/cmake"; then
-    CMAKE = /Applications/CMake.app/Contents/bin/cmake
+    CMAKE=/Applications/CMake.app/Contents/bin/cmake
 fi
 
 mkdir -p ../../solutions/downloads

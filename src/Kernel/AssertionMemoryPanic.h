65a5e4f0e41d39d23fb7187cf5424df149933ea3
irov <irov13@mail.ru>
2020-07-07 15:31:32 +0300

    fix Release build

--- src/Kernel/AssertionMemoryPanic.h
+++ src/Kernel/AssertionMemoryPanic.h
@@ -6,6 +6,6 @@
 #ifdef MENGINE_ASSERTION_DEBUG
 #   define MENGINE_ASSERTION_MEMORY_PANIC( memory, ... ) if( (memory) == nullptr ) Helper::AssertionOperator( ASSERTION_LEVEL_FATAL, #memory " == nullptr", MENGINE_CODE_FUNCTION, MENGINE_CODE_LINE )(__VA_ARGS__)
 #else
-#   define MENGINE_ASSERTION_MEMORY_PANIC_( memory, ... )
+#   define MENGINE_ASSERTION_MEMORY_PANIC( memory, ... )
 #endif
 //////////////////////////////////////////////////////////////////////////

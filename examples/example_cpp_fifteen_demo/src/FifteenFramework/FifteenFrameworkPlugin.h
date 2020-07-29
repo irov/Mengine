9f4aae66af0d351c5555792e4605b986b43f17d8
goku <goku@syneforge.com>
2020-07-29 16:22:31 +0300

    add example_cpp_fifteen_demo

---
+++ examples/example_cpp_fifteen_demo/src/FifteenFramework/FifteenFrameworkPlugin.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Kernel/PluginBase.h"
+
+namespace Mengine
+{
+    //////////////////////////////////////////////////////////////////////////
+    class FifteenFrameworkPlugin
+        : public PluginBase
+    {
+        PLUGIN_DECLARE( "FifteenFramework" )
+
+    public:
+        FifteenFrameworkPlugin();
+        ~FifteenFrameworkPlugin() override;
+
+    protected:
+        bool _initializePlugin() override;
+        void _finalizePlugin() override;
+    };
+    //////////////////////////////////////////////////////////////////////////
+}

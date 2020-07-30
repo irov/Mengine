53f9493dbeb6f7ce989108d686d6ee0effeafb0a
irov <irov13@mail.ru>
2020-07-30 18:55:51 +0300

    improve build bat

--- build/mingw_x64/build_solution_mingw_x64.bat
+++ build/mingw_x64/build_solution_mingw_x64.bat
@@ -5,6 +5,9 @@ if ["%~1"]==[""] (
   goto end
 )
 
+setlocal
+call :setESC
+
 set "CONFIGURATION=%1"
 set "EXTERNAL_PDB_PATH=%2"
 set "BUILD_VERSION=%3"
@@ -15,7 +18,23 @@ set "BUILD_VERSION=%3"
 @call build_solution.bat "SOLUTION_NAME=solution_mingw64" "SOURCE_DIRECTORY=%CD%\..\cmake\MinGW64" "GENERATOR=Ninja" "CONFIGURATION=%CONFIGURATION%" "EXTERNAL_PDB_PATH=%EXTERNAL_PDB_PATH%" "BUILD_VERSION=%BUILD_VERSION%"
 @popd
 
+if errorlevel 1 (
+    @echo %ESC%[91m*****************************************%ESC%[0m
+    @echo %ESC%[91m***************  Failure  ***************%ESC%[0m
+    @echo %ESC%[91m*****************************************%ESC%[0m
+) else (
+    @echo %ESC%[92m=========================================%ESC%[0m
+    @echo %ESC%[92m=============  Successful  ==============%ESC%[0m
+    @echo %ESC%[92m=========================================%ESC%[0m
+)
+
 :end
 @echo Done
 
 @pause
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)

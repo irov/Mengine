5728e4cb87640cacbdaa38516816a6bec756a6a9
irov <irov13@mail.ru>
2020-07-30 18:55:51 +0300

    improve build bat

--- build/downloads/downloads_windows.bat
+++ build/downloads/downloads_windows.bat
@@ -1,5 +1,8 @@
 @echo off
 
+setlocal
+call :setESC
+
 @echo Starting download dependencies...
 
 @mkdir %~dp0..\..\solutions\downloads
@@ -11,6 +14,22 @@
 @call CMake.exe --build . -j 4 -- /verbosity:minimal
 @popd
 
+if errorlevel 1 (
+    @echo %ESC%[91m*****************************************%ESC%[0m
+    @echo %ESC%[91m***************  Failure  ***************%ESC%[0m
+    @echo %ESC%[91m*****************************************%ESC%[0m
+) else (
+    @echo %ESC%[92m=========================================%ESC%[0m
+    @echo %ESC%[92m=============  Successful  ==============%ESC%[0m
+    @echo %ESC%[92m=========================================%ESC%[0m
+)
+
 @echo Done
 
 @pause
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)

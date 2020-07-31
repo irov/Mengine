cff89e1ec79583c7b7eca08fb0daf6f4ab59fc91
lenin <lenin@syneforge.com>
2020-07-31 12:33:15 +0300

    add puzzle 15 example

---
+++ examples/example_cpp_15_puzzle_demo/src/PuzzleFramework/PuzzleFramework.cpp
@@ -0,0 +1,47 @@
+#include "PuzzleFramework.h"
+
+#include "Interface/PrototypeServiceInterface.h"
+#include "Interface/SceneServiceInterface.h"
+
+#include "Kernel/FactorableUnique.h"
+#include "Kernel/EntityEventable.h"
+#include "Kernel/Logger.h"
+#include "Kernel/Scene.h"
+#include "Kernel/SceneHelper.h"
+#include "Kernel/Document.h"
+#include "Kernel/ConstStringHelper.h"
+
+#include "PuzzleSceneEventReceiver.h"
+
+//////////////////////////////////////////////////////////////////////////
+namespace Mengine
+{
+    //////////////////////////////////////////////////////////////////////////
+    PuzzleFramework::PuzzleFramework()
+    {
+    }
+    //////////////////////////////////////////////////////////////////////////
+    PuzzleFramework::~PuzzleFramework()
+    {
+    }
+    //////////////////////////////////////////////////////////////////////////
+    bool PuzzleFramework::_initializeFramework()
+    {
+        PuzzleSceneEventReceiverPtr sceneEventReceiver = Helper::makeFactorableUnique<PuzzleSceneEventReceiver>( MENGINE_DOCUMENT_FACTORABLE );
+
+        ScenePtr scene = Helper::makeScene( sceneEventReceiver, MENGINE_DOCUMENT_FACTORABLE );
+
+        scene->setName( STRINGIZE_STRING_LOCAL( "Puzzle" ) );
+
+        SCENE_SERVICE()
+            ->setCurrentScene( scene, false, false, nullptr );
+
+        return true;
+    }
+    //////////////////////////////////////////////////////////////////////////
+    void PuzzleFramework::_finalizeFramework()
+    {
+        SCENE_SERVICE()
+            ->removeCurrentScene( true, nullptr );
+    }
+}
---
+++ examples/example_cpp_15_puzzle_demo/src/PuzzleFramework/PuzzleFrameworkPlugin.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Kernel/PluginBase.h"
+
+namespace Mengine
+{
+    class PuzzleFrameworkPlugin
+        : public PluginBase
+    {
+        PLUGIN_DECLARE( "PuzzleFramework" )
+
+    public:
+        PuzzleFrameworkPlugin();
+        ~PuzzleFrameworkPlugin() override;
+
+    protected:
+        bool _initializePlugin() override;
+        void _finalizePlugin() override;
+    };
+}

cb9778abdf5f2e6b7645c61df0c54796bdcfeaf5
lenin <lenin@syneforge.com>
2020-07-31 12:33:15 +0300

    add puzzle 15 example

---
+++ examples/example_cpp_15_puzzle_demo/src/PuzzleFramework/PuzzleFramework.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Kernel/FrameworkBase.h"
+#include "Kernel/Observable.h"
+
+namespace Mengine
+{
+    class PuzzleFramework
+        : public FrameworkBase
+    {
+    public:
+        PuzzleFramework();
+        ~PuzzleFramework() override;
+
+    protected:
+        bool _initializeFramework() override;
+        void _finalizeFramework() override;
+    };
+}

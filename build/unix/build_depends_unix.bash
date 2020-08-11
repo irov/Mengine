2f2e908503142185dd58d2807a86b840b624f931
irov <irov13@mail.ru>
2020-08-11 13:18:05 +0300

    improve Unix build

--- build/unix/build_depends_unix.bash
+++ build/unix/build_depends_unix.bash
@@ -4,6 +4,6 @@ CONFIGURATION=$1
 
 mkdir -p ../../solutions/dependencies_unix/$CONFIGURATION
 pushd ../../solutions/dependencies_unix/$CONFIGURATION
-cmake -G "Unix Makefiles" -S "../../../cmake/Depends_Unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
+cmake -G "Unix Makefiles" -S "../../../cmake/Depends_Unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION  -DCMAKE_CONFIGURATION_TYPES:STRING=$CONFIGURATION
 cmake --build ./ --config $CONFIGURATION
 popd

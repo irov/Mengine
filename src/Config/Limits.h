d7fe261c6892c362318fc330028d9789790f0366
irov <irov13@mail.ru>
2020-08-14 17:01:34 +0300

    fix Limits

--- src/Config/Limits.h
+++ src/Config/Limits.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <limits>
+#include <limits.h>
 
-#define MENGINE_INT_MIN INT_MIN
-#define MENGINE_INT_MAX INT_MAX
+#define MENGINE_INT_MIN (INT_MIN)
+#define MENGINE_INT_MAX (INT_MAX)
 
-#define MENGINE_FLT_MIN FLT_MIN
-#define MENGINE_FLT_MAX FLT_MAX
+#define MENGINE_FLT_MIN (FLT_MIN)
+#define MENGINE_FLT_MAX (FLT_MAX)

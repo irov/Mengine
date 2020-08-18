c1e73d52468933cb880d4ea2f47f0bec5e84454e
Yuriy <irov13@mail.ru>
2020-08-19 00:52:22 +0300

    improve LoggerService initial log

--- src/Services/PrefetcherService/ThreadTaskPrefetchImageDecoder.h
+++ src/Services/PrefetcherService/ThreadTaskPrefetchImageDecoder.h
@@ -6,6 +6,7 @@
 
 namespace Mengine
 {
+    //////////////////////////////////////////////////////////////////////////
     class ThreadTaskPrefetchImageDecoder
         : public ThreadTaskPrefetch
     {

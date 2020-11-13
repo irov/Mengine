0bf7ed5c4f213dd2045e0cd348ca678d3f04e3d2
Yuriy <irov13@mail.ru>
2020-11-13 02:24:09 +0200

    improve mingw for x86

--- cmake/mingw_template.cmake
+++ cmake/mingw_template.cmake
@@ -5,8 +5,8 @@ set(CMAKE_DEBUG_POSTFIX "")
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -femulated-tls -DWIN32")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -femulated-tls -DWIN32")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -femulated-tls -DWIN32 -m32")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -femulated-tls -DWIN32 -m32")
 
 set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g -fno-strict-aliasing -fomit-frame-pointer -std=c11 -O0 -D_DEBUG")
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -fno-strict-aliasing -fomit-frame-pointer -std=c11 -O3 -DNDEBUG")

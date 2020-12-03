c5b7989829e303110a5d665fdff6d852bd60a66b
Yuriy <irov13@mail.ru>
2020-12-03 02:42:47 +0200

    improve StdIntTypes.h

--- src/Config/StdIntTypes.h
+++ src/Config/StdIntTypes.h
@@ -1,4 +1,30 @@
 #pragma once
 
+#include "Config/Config.h"
+
 #define __STDC_FORMAT_MACROS
 #include <cinttypes>
+
+#ifdef MENGINE_PLATFORM_WINDOWS64
+#define MENGINE_PRDWORD "lu"
+#else
+#define MENGINE_PRDWORD "u"
+#endif
+
+#ifdef MENGINE_PLATFORM_WINDOWS64
+#define MENGINE_PRDWORD_PTR "llu"
+#else
+#define MENGINE_PRDWORD_PTR "lu"
+#endif
+
+#ifdef MENGINE_PLATFORM_WINDOWS64
+#define MENGINE_PRWPARAM "llu"
+#else
+#define MENGINE_PRWPARAM "lu"
+#endif
+
+#ifdef MENGINE_PLATFORM_WINDOWS64
+#define MENGINE_PRLPARAM "lld"
+#else
+#define MENGINE_PRLPARAM "ld"
+#endif

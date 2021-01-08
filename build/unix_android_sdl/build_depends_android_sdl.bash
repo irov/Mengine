5b5c1c3dba4c0869537a1e8396da9fbf6cd10fdb
Yuriy <irov13@mail.ru>
2021-01-08 05:10:17 +0200

    wip circleci improve gradle OpenAL32

--- build/unix_android_sdl/build_depends_android_sdl.bash
+++ build/unix_android_sdl/build_depends_android_sdl.bash
@@ -9,44 +9,30 @@ CMAKE_GENERATOR="Ninja"
 CMAKELIST_PATH=$PWD/../../cmake/Depends_Android_SDL
 SOLUTION_DIR=$PWD/../../solutions/dependencies_android_sdl
 
-mkdir -p $SOLUTION_DIR/$BUILD_TYPE/armeabi-v7a
-pushd $SOLUTION_DIR/$BUILD_TYPE/armeabi-v7a
+build_dependencies () {
+    mkdir -p $SOLUTION_DIR/$BUILD_TYPE/$1
+    pushd $SOLUTION_DIR/$BUILD_TYPE/$1
 
-cmake -G $CMAKE_GENERATOR \
-    -DANDROID_PLATFORM=$ANDROID_PLATFORM \
-    -DANDROID_ARM_NEON=TRUE \
-    -DANDROID_ABI=armeabi-v7a \
-    -DANDROID_STL=c++_shared \
-    -DANDROID_TOOLCHAIN=clang \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_CONFIGURATION_TYPES:STRING=$BUILD_TYPE \
-    -S $CMAKELIST_PATH
+    cmake -G $CMAKE_GENERATOR \
+        -DANDROID_PLATFORM=$ANDROID_PLATFORM \
+        -DANDROID_ARM_NEON=TRUE \
+        -DANDROID_ABI=$1 \
+        -DANDROID_STL=c++_shared \
+        -DANDROID_TOOLCHAIN=clang \
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+        -DCMAKE_CONFIGURATION_TYPES:STRING=$BUILD_TYPE \
+        -S $CMAKELIST_PATH
 
-cmake --build . --config $BUILD_TYPE
+    cmake --build . --config $BUILD_TYPE
+    
+    status=$?
 
-status=$?
+    popd
 
-popd
+    echo $status
+}
 
-[ $status -eq 0 ]  || exit 1
-
-mkdir -p $SOLUTION_DIR/$BUILD_TYPE/arm64-v8a
-pushd $SOLUTION_DIR/$BUILD_TYPE/arm64-v8a
-
-cmake -G $CMAKE_GENERATOR \
-    -DANDROID_PLATFORM=$ANDROID_PLATFORM \
-    -DANDROID_ARM_NEON=TRUE \
-    -DANDROID_ABI=arm64-v8a \
-    -DANDROID_STL=c++_shared \
-    -DANDROID_TOOLCHAIN=clang \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_CONFIGURATION_TYPES:STRING=$BUILD_TYPE \
-    -S $CMAKELIST_PATH
-
-cmake --build . --config $BUILD_TYPE
-
-status=$?
-
-popd
-
-[ $status -eq 0 ]  || exit 1
+[ build_dependencies x86 -eq 0 ]  || exit 1
+[ build_dependencies x86_64 -eq 0 ]  || exit 1
+[ build_dependencies armeabi-v7a -eq 0 ]  || exit 1
+[ build_dependencies arm64-v8a -eq 0 ]  || exit 1

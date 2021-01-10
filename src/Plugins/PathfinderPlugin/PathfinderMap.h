ef5a5db0ebb2317f7fdbe776d1d58d908c22c77b
Yuriy <irov13@mail.ru>
2021-01-11 01:47:39 +0200

    improve PathfinderMap rework gradle build SDL2 to CMake

--- src/Plugins/PathfinderPlugin/PathfinderMap.h
+++ src/Plugins/PathfinderPlugin/PathfinderMap.h
@@ -5,6 +5,23 @@
 namespace Mengine
 {
     //////////////////////////////////////////////////////////////////////////
+    struct BresenhamPoint
+    {
+        uint16_t x;
+        uint16_t y;
+    };
+    //////////////////////////////////////////////////////////////////////////
+    struct BresenhamLine
+    {
+        int16_t deltaX;
+        int16_t deltaY;
+
+        int16_t signX;
+        int16_t signY;
+
+        int16_t error;
+    };
+    //////////////////////////////////////////////////////////////////////////
     class PathfinderMap
         : public PathfinderMapInterface
     {
@@ -33,6 +50,9 @@ namespace Mengine
     protected:
         bool walkerBresenham_( uint16_t _fromX, uint16_t _fromY, uint16_t _toX, uint16_t _toY, PathfinderPathDesc * const _path ) const;
         bool makeFunnels_( uint16_t _fromX, uint16_t _fromY, uint16_t _toX, uint16_t _toY ) const;
+        void stepFunnels_( uint16_t _x, uint16_t _y, uint16_t _finalX, uint16_t _finalY, uint16_t _layer ) const;
+        bool testStep( uint16_t _fromX, uint16_t _fromY, uint16_t _toX, uint16_t _toY, uint16_t _layer ) const;
+        MENGINE_INLINE bool testFunnelCost_( uint16_t _x, uint16_t _y, uint16_t _cost ) const;
 
     protected:
         uint16_t m_width;
@@ -48,17 +68,37 @@ namespace Mengine
         typedef Vector<CellDesc> VectorCells;
         VectorCells m_cells;
 
-        typedef Vector<uint16_t> VectorFunnels;
-        VectorFunnels m_funnels;
+        typedef Vector<uint32_t> VectorFunnels;
+        mutable VectorFunnels m_funnels;
+
+        typedef Vector<BresenhamPoint> VectorFunnelSteps;
+        mutable VectorFunnelSteps m_steps[8];
+
+        mutable uint16_t m_iterators[8] = {0};
+
+        mutable uint32_t m_finalCost;
 
     protected:
-        MENGINE_INLINE const CellDesc & getCellDesc_( uint16_t _fromX, uint16_t _fromY ) const;
+        MENGINE_INLINE const CellDesc & getCellDesc_( uint16_t _x, uint16_t _y ) const;
+        MENGINE_INLINE void setFunnelCost_( uint16_t _x, uint16_t _y, uint32_t _cost ) const;
+        MENGINE_INLINE uint32_t getFunnelCost_( uint16_t _x, uint16_t _y ) const;
     };
     //////////////////////////////////////////////////////////////////////////
     typedef IntrusivePtr<PathfinderMap> PathfinderMapPtr;
     //////////////////////////////////////////////////////////////////////////
-    MENGINE_INLINE const PathfinderMap::CellDesc & PathfinderMap::getCellDesc_( uint16_t _fromX, uint16_t _fromY ) const
+    MENGINE_INLINE const PathfinderMap::CellDesc & PathfinderMap::getCellDesc_( uint16_t _x, uint16_t _y ) const
     {
-        return m_cells[_fromX + _fromY * m_width];
+        return m_cells[_x + _y * m_width];
     }
+    //////////////////////////////////////////////////////////////////////////
+    MENGINE_INLINE void PathfinderMap::setFunnelCost_( uint16_t _x, uint16_t _y, uint32_t _cost ) const
+    {
+        m_funnels[_x + _y * m_width] = _cost;
+    }
+    //////////////////////////////////////////////////////////////////////////
+    MENGINE_INLINE uint32_t PathfinderMap::getFunnelCost_( uint16_t _x, uint16_t _y ) const
+    {
+        return m_funnels[_x + _y * m_width];
+    }
+    //////////////////////////////////////////////////////////////////////////
 }

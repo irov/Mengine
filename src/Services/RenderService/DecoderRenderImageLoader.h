943cc1cf5ef78d51a1d0a2e3b3f3153c8a7c32b5
irov <irov13@mail.ru>
2021-02-26 15:41:04 +0200

    improve clear TextField add RenderImageLoaderInterface getMemory

--- src/Services/RenderService/DecoderRenderImageLoader.h
+++ src/Services/RenderService/DecoderRenderImageLoader.h
@@ -29,6 +29,9 @@ namespace Mengine
         bool load( const RenderImageInterfacePtr & _image ) const override;
 
     protected:
+        MemoryInterfacePtr getMemory( uint32_t _codecFlags, const DocumentPtr & _doc ) const override;
+
+    protected:
         ImageDecoderInterfacePtr createImageDecoder_( const FileGroupInterfacePtr & _fileGroup, const FilePath & _filePath, const ConstString & _codecType ) const;
 
     protected:

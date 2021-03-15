93e70f2f5dfc1476680939f082dc178915f44671
irov <irov13@mail.ru>
2021-03-15 19:15:31 +0200

    fix MixinDebug

--- src/Kernel/MixinDebug.h
+++ src/Kernel/MixinDebug.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Config/Config.h"
+#include "Config/UniqueId.h"
 
 #if defined(MENGINE_DEBUG)
 #   include "Kernel/Identity.h"
@@ -9,12 +10,12 @@
 namespace Mengine
 {    
 #if defined(MENGINE_DEBUG)
-#   define MENGINE_MIXIN_DEBUG_NAME(Mixin) (dynamic_cast<const Identity *>(Mixin) != nullptr ? dynamic_cast<const Identity *>(Mixin)->getName().c_str() : "UNKNOWN")
-#   define MENGINE_MIXIN_DEBUG_TYPE(Mixin) (dynamic_cast<const Identity *>(Mixin) != nullptr ? dynamic_cast<const Identity *>(Mixin)->getType().c_str() : "UNKNOWN")
-#   define MENGINE_MIXIN_DEBUG_UID(Mixin) (dynamic_cast<const Identity *>(Mixin) != nullptr ? dynamic_cast<const Identity *>(Mixin)->getUniqueIdentity() : INVALIDATE_UNIQUE_ID)
+#   define MENGINE_MIXIN_DEBUG_NAME(Mixin) (dynamic_cast<const Mengine::Identity *>(Mixin) != nullptr ? dynamic_cast<const Mengine::Identity *>(Mixin)->getName().c_str() : "UNKNOWN")
+#   define MENGINE_MIXIN_DEBUG_TYPE(Mixin) (dynamic_cast<const Mengine::Identity *>(Mixin) != nullptr ? dynamic_cast<const Mengine::Identity *>(Mixin)->getType().c_str() : "UNKNOWN")
+#   define MENGINE_MIXIN_DEBUG_UID(Mixin) (dynamic_cast<const Mengine::Identity *>(Mixin) != nullptr ? dynamic_cast<const Mengine::Identity *>(Mixin)->getUniqueIdentity() : Mengine::INVALIDATE_UNIQUE_ID)
 #else
 #   define MENGINE_MIXIN_DEBUG_NAME(Mixin) ("UNKNOWN")
 #   define MENGINE_MIXIN_DEBUG_TYPE(Mixin) ("UNKNOWN")
-#   define MENGINE_MIXIN_DEBUG_UID(Mixin) (INVALIDATE_UNIQUE_ID)
+#   define MENGINE_MIXIN_DEBUG_UID(Mixin) (Mengine::INVALIDATE_UNIQUE_ID)
 #endif
 }

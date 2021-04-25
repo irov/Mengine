5d85b2f4be26876f67709d915ab3d2a73c1bd905
Yuriy <irov13@mail.ru>
2021-04-25 21:30:01 +0300

    wip dx11 shaders

--- examples/shaders/Materials/DX11_Fragment_Blend.hlsl
+++ examples/shaders/Materials/DX11_Fragment_Blend.hlsl
@@ -4,18 +4,17 @@ SamplerState sampler0 : register(s0);
 
 struct v2p {
     float4 position  : SV_POSITION;
-    float4 color : COLOR0;
-    float2 tex0 : TEXCOORD0;
+    float4 color     : COLOR0;
+    float2 uv0       : TEXCOORD0;
 };
 
 // Per-pixel color data passed through the pixel shader.
 struct PixelShaderInput
 {
 	min16float4 pos     : SV_POSITION;
-	min16float2 color   : COLOR0;
-	uint        tex0    : TEXCOORD0;
+	min16float4 color   : COLOR0;
+	min16float2 uv0     : TEXCOORD0;
 };
-
 
 struct PixelShaderOutput {
 	min16float4 color : COLOR0;
@@ -24,6 +23,6 @@ struct PixelShaderOutput {
 void main(in PixelShaderInput IN, out PixelShaderOutput OUT)
 {
 	//Lookup texture color
-	min16float4 TexColor = tex0.Sample(sampler0, IN.tex0);
+	min16float4 TexColor = tex0.Sample(sampler0, IN.uv0);
     OUT.color = IN.color * TexColor;
 }

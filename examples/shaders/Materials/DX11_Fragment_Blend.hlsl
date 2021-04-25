d2c7ad38222aaa8358fb8e66029a4b3fe874a0fa
Oleg Ferapontov <oleg.osiris.ferapontov@gmail.com>
2021-04-25 16:16:40 +0300

    d3d11 blend

---
+++ examples/shaders/Materials/DX11_Fragment_Blend.hlsl
@@ -0,0 +1,29 @@
+// textures
+Texture2D	tex0 : register(t0);
+SamplerState sampler0 : register(s0);
+
+struct v2p {
+    float4 position  : SV_POSITION;
+    float4 color : COLOR0;
+    float2 tex0 : TEXCOORD0;
+};
+
+// Per-pixel color data passed through the pixel shader.
+struct PixelShaderInput
+{
+	min16float4 pos     : SV_POSITION;
+	min16float2 color   : COLOR0;
+	uint        tex0    : TEXCOORD0;
+};
+
+
+struct PixelShaderOutput {
+	min16float4 color : COLOR0;
+};
+  
+void main(in PixelShaderInput IN, out PixelShaderOutput OUT)
+{
+	//Lookup texture color
+	min16float4 TexColor = tex0.Sample(sampler0, IN.tex0);
+    OUT.color = IN.color * TexColor;
+}

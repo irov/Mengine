2c0a04501c166c9994ba2a6c91f12ab77b0f4a75
Yuriy <irov13@mail.ru>
2021-05-24 03:48:02 +0300

    fix build_depends_msvc16_dx11

--- build/msvc16_dx11/build_depends_msvc16_dx11.bat
+++ build/msvc16_dx11/build_depends_msvc16_dx11.bat
@@ -17,7 +17,7 @@ set "YEAR=2019"
 @popd
 
 @pushd %~dp0..
-@call build_depends.bat "SOLUTION_NAME=dependencies_msvc%VERSION%_dx11" "SOURCE_DIRECTORY=%CD%\..\cmake\Depends_WIN32_DX11" "GENERATOR=Visual Studio %VERSION% %YEAR%" "CONFIGURATION=%CONFIGURATION%" "VERBOSITY=minimal"
+@call build_depends.bat "SOLUTION_NAME=dependencies_msvc%VERSION%_dx11" "SOURCE_DIRECTORY=%CD%\..\cmake\Depends_WIN32_DX11" "GENERATOR=Visual Studio %VERSION% %YEAR%" "CONFIGURATION=%CONFIGURATION%" "ARCHITECTURE=Win32" "VERBOSITY=minimal"
 @popd
 
 :end

43a37b1ec85d5b2b09985f965ab0ae443892929d
Oleg Ferapontov <oleg.osiris.ferapontov@gmail.com>
2021-06-11 12:59:40 +0300

    render target creating fix sampler state linear filtering added

--- src/Systems/DX11RenderSystem/DX11RenderTargetTexture.cpp
+++ src/Systems/DX11RenderSystem/DX11RenderTargetTexture.cpp
@@ -36,9 +36,13 @@ namespace Mengine
         m_textureDesc.Width = _width;
         m_textureDesc.Height = _height;
         m_textureDesc.MipLevels = 1;
-        m_textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		m_textureDesc.ArraySize = 1;
+		m_textureDesc.SampleDesc.Count = 1;
+		m_textureDesc.SampleDesc.Quality = 0;
+        m_textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
         m_textureDesc.Usage = D3D11_USAGE_DEFAULT;
-
+		m_textureDesc.CPUAccessFlags = 0;
+		m_textureDesc.MiscFlags = 0;
         m_hwPixelFormat = _format;
 
         m_hwWidthInv = 1.f / (float)m_textureDesc.Width;

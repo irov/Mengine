39d6e921e6ed4dd0ea3042ad262fd83de935504a
irov <irov13@mail.ru>
2022-04-14 23:01:32 +0300

    improve AppleMARSDKDelegate

--- src/Plugins/AppleMARSDKPlugin/AppleMARSDKCallbacksProtocol.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol AppleMARSDKCallbacksProtocol <NSObject>
-- (void) onUserLogin:(NSDictionary* _Nullable) params;
-- (void) onUserLogout:(NSDictionary* _Nullable) params;
-- (void) onPayPaid:(NSDictionary* _Nullable) params;
-@end

2e38c7578f3a471b9e6285e95767c84c117194ce
irov <irov13@mail.ru>
2022-04-21 14:56:38 +0300

    fix ResourcePrefetcherPlugin

--- src/Plugins/ResourcePrefetcherPlugin/ResourcePrefetcherPlugin.cpp
+++ src/Plugins/ResourcePrefetcherPlugin/ResourcePrefetcherPlugin.cpp
@@ -1,5 +1,8 @@
 #include "ResourcePrefetcherPlugin.h"
 
+#include "Kernel/ConfigHelper.h"
+#include "Kernel/OptionHelper.h"
+
 //////////////////////////////////////////////////////////////////////////
 SERVICE_EXTERN( ResourcePrefetcherService );
 //////////////////////////////////////////////////////////////////////////
@@ -14,6 +17,24 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     ResourcePrefetcherPlugin::~ResourcePrefetcherPlugin()
     {
+    }
+    //////////////////////////////////////////////////////////////////////////
+    bool ResourcePrefetcherPlugin::_availablePlugin() const
+    {
+        if( HAS_OPTION( "noresourceprefetcher" ) == true )
+        {
+            return false;
+        }
+
+        if( HAS_OPTION( "resourceprefetcher" ) == false )
+        {
+            if( CONFIG_VALUE( "ResourcePrefetcher", "Enable", true ) == false )
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
     //////////////////////////////////////////////////////////////////////////
     bool ResourcePrefetcherPlugin::_initializePlugin()
--- src/Plugins/ResourcePrefetcherPlugin/ResourcePrefetcherPlugin.h
+++ src/Plugins/ResourcePrefetcherPlugin/ResourcePrefetcherPlugin.h
@@ -14,6 +14,7 @@ namespace Mengine
         ~ResourcePrefetcherPlugin() override;
 
     protected:
+        bool _availablePlugin() const override;
         bool _initializePlugin() override;
         void _finalizePlugin() override;
         void _destroyPlugin() override;

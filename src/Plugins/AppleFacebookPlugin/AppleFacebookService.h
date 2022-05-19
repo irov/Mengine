8df16e3b47486440525e6db749c741f8c9a6991e
Alexandr <trbrmrdr@gmail.com>
2022-05-19 22:04:24 +0300

    fix AppleFacebookService

--- src/Plugins/AppleFacebookPlugin/AppleFacebookService.h
+++ src/Plugins/AppleFacebookPlugin/AppleFacebookService.h
@@ -1,12 +1,16 @@
 #pragma once
 
-#include "ApplleFacebookInterface.h"
+#include "AppleFacebookInterface.h"
 
 #include "Kernel/ServiceBase.h"
 
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import <FBSDKShareKit/FBSDKShareKit.h>
+
+#include "Kernel/ConstString.h"
+
+
 
 namespace Mengine
 {
@@ -22,7 +26,25 @@ namespace Mengine
         void _finalizeService() override;
         
     public:
-        void login();
+        virtual void setProvider( const AppleFacebookProviderInterfacePtr & _provider ) override;
+        virtual const AppleFacebookProviderInterfacePtr & getProvider() const override;
+        
+    public:
+        virtual void login() override;
+        virtual void logout() override;
+        virtual bool isLoggedIn() const override;
+        virtual const char* getAccessToken() const override;
+        virtual void shareLink(const char* link,const char* picture) override;
+        virtual void getProfilePictureLink() override;
+        
+    private:
+        AppleFacebookProviderInterfacePtr m_provider;
+        FBSDKLoginManager *m_loginManager;
+        
+        bool m_isProcessed;
+        NSString *m_userID;
+        NSString *m_imageUrl;
+        
         
     };
 }

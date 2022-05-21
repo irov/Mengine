219fa4437727784453256f515c001b8f42056c73
irov <irov13@mail.ru>
2022-05-21 23:31:29 +0300

    fix release python crash

--- src/Environment/Python/PythonIncluder.h
+++ src/Environment/Python/PythonIncluder.h
@@ -4,7 +4,10 @@
 
 #ifdef MENGINE_USE_SCRIPT_SERVICE
 #   define PYBIND_STL_SUPPORT
-#   define PYBIND_CALL_DEBUG
+
+#   ifdef MENGINE_DEBUG
+#       define PYBIND_CALL_DEBUG
+#   endif
 
 #   include "pybind/pybind.hpp"
 #endif

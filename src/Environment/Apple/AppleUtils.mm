eec9f1e6516c5f21584e4afb13b51e1aa0d087d4
irov <irov13@mail.ru>
2022-09-11 20:01:18 +0300

    wip

--- src/Environment/Apple/AppleUtils.mm
+++ src/Environment/Apple/AppleUtils.mm
@@ -1,7 +1,5 @@
 #include "AppleUtils.h"
 
-#import <Foundation/Foundation.h>
-
 #if defined(MENGINE_PLATFORM_MACOS)
 #   import <AppKit/AppKit.h>
 #else
@@ -33,30 +31,6 @@ namespace Mengine
             }
         #endif
             return true;
-        }
-        /////////////////////////////////////////////////////////////////////////
-        NSErrorMessage AppleGetMessageFromNSError( NSError * _error )
-        {
-            NSString * message = [NSString stringWithFormat:@"[Error %ld Description: %@ Failure reason: %@ Recovery suggestion: %@"
-                , [_error code]
-                , [_error localizedDescription]
-                , [_error localizedFailureReason]
-                , [_error localizedRecoverySuggestion]
-                ];
-
-            const Char * message_str = [message UTF8String];
-
-            return NSErrorMessage( message_str );
-        }
-        /////////////////////////////////////////////////////////////////////////
-        void AppleGetMapNSDictionary( NSDictionary * _dictionary, Map<String, String> * const _map )
-        {
-            [_dictionary enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL *stop) {
-                const Char * key_str = [[NSString stringWithFormat:@"%@", key] UTF8String];
-                const Char * value_str = [[NSString stringWithFormat:@"%@", value] UTF8String];
-                
-                _map->emplace(std::make_pair(String(key_str), String(value_str)));
-            }];
         }
         //////////////////////////////////////////////////////////////////////////
     }
--- src/Plugins/AppleFacebookPlugin/AppleFacebookService.mm
+++ src/Plugins/AppleFacebookPlugin/AppleFacebookService.mm
@@ -1,6 +1,6 @@
 #include "AppleFacebookService.h"
 
-#include "Environment/Apple/AppleUtils.h"
+#include "Environment/Apple/AppleErrorHelper.h"
 
 #include "Kernel/Logger.h"
 

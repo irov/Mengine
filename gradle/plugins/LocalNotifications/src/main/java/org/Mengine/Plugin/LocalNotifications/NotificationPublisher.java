6491db5860a70817160f2c7820cf6a055139eb10
irov <irov13@mail.ru>
2023-02-24 15:48:48 +0200

    fix android NotificationPublisher

--- gradle/plugins/LocalNotifications/src/main/java/org/Mengine/Plugin/LocalNotifications/NotificationPublisher.java
+++ gradle/plugins/LocalNotifications/src/main/java/org/Mengine/Plugin/LocalNotifications/NotificationPublisher.java
@@ -13,7 +13,13 @@ public class NotificationPublisher extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        Notification notification = intent.getParcelableExtra(NOTIFICATION, Notification.class);
+        Notification notification;
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            notification = intent.getParcelableExtra(NOTIFICATION, Notification.class);
+        } else {
+            notification = intent.getParcelableExtra(NOTIFICATION);
+        }
+
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
         notificationManager.notify(id, notification);
     }

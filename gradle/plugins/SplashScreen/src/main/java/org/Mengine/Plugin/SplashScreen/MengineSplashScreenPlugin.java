574b837651acdbce021d89bcb29d7b0233672657
irov <irov13@mail.ru>
2023-02-25 21:23:26 +0200

    improve android plugin SplashScreen

--- gradle/plugins/SplashScreen/src/main/java/org/Mengine/Plugin/SplashScreen/MengineSplashScreenPlugin.java
+++ gradle/plugins/SplashScreen/src/main/java/org/Mengine/Plugin/SplashScreen/MengineSplashScreenPlugin.java
@@ -3,15 +3,18 @@ package org.Mengine.Plugin.SplashScreen;
 import org.Mengine.Base.MenginePlugin;
 import org.Mengine.Base.MengineActivity;
 
-import android.view.MotionEvent;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 
 public class MengineSplashScreenPlugin extends MenginePlugin {
     public static final String PLUGIN_NAME = "SplashScreen";
@@ -22,7 +25,8 @@ public class MengineSplashScreenPlugin extends MenginePlugin {
     @Override
     public void onCreate(MengineActivity activity, Bundle savedInstanceState) {
         ImageView image = new ImageView(activity);
-        image.setBackgroundResource(R.drawable.splashscreen);
+        Drawable mengine_splashscreen = activity.getContext().getResources().getDrawable(R.drawable.mengine_splashscreen);
+        image.setBackground(mengine_splashscreen);
         image.setScaleType(ImageView.ScaleType.CENTER_CROP);
         ViewGroup.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
         image.setLayoutParams(params);
@@ -32,79 +36,82 @@ public class MengineSplashScreenPlugin extends MenginePlugin {
 
         m_image = image;
 
-        this.showSplash();
+        this.showSplash(activity);
     }
 
     @Override
     public void onMengineApplicationReady(MengineActivity activity) {
-        this.hideSplash();
+        this.hideSplash(activity);
     }
 
-    public void showSplash() {
-        this.logMessage("showSplash");
-
-        m_image.clearAnimation();
-
-        AlphaAnimation animation = new AlphaAnimation(0.f, 1.f);
-        animation.setDuration(R.integer.splashscreen_show_duration);
-        animation.setFillAfter(true);
-        animation.setAnimationListener(new Animation.AnimationListener() {
+    private void showSplash(@NonNull MengineActivity activity) {
+        AlphaAnimation showAnimation = new AlphaAnimation(0.f, 1.f);
+        long durationMillis = activity.getContext().getResources().getInteger(R.integer.mengine_splashscreen_show_duration);
+        showAnimation.setDuration(durationMillis);
+        showAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
+                MengineSplashScreenPlugin.this.logMessage("show splash screen");
+
                 m_image.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                m_image.setVisibility(View.VISIBLE);
+                MengineSplashScreenPlugin.this.logMessage("shown splash screen");
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
-                //Empty
             }
         });
 
-        m_image.startAnimation(animation);
+        m_image.startAnimation(showAnimation);
     }
 
-    public void hideSplash() {
-        this.logMessage("hideSplash");
+    private void hideSplash(@NonNull MengineActivity activity) {
+        Animation showAnimation = m_image.getAnimation();
 
-        m_image.clearAnimation();
+        float alphaFrom = 1.f;
+        if (showAnimation != null) {
+            long drawTime = m_image.getDrawingTime();
+            Transformation transformation = new Transformation();
+            showAnimation.getTransformation(drawTime, transformation);
+            float imageAlpha = transformation.getAlpha();
+            alphaFrom = imageAlpha;
+        }
 
-        AlphaAnimation animation = new AlphaAnimation(1.f, 0.f);
-        animation.setDuration(R.integer.splashscreen_hide_duration);
-        animation.setFillAfter(true);
-        animation.setAnimationListener(new Animation.AnimationListener() {
+        AlphaAnimation hideAnimation = new AlphaAnimation(alphaFrom, 0.f);
+        long durationMillis = activity.getContext().getResources().getInteger(R.integer.mengine_splashscreen_hide_duration);
+        hideAnimation.setDuration(durationMillis);
+        hideAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                m_image.setVisibility(View.VISIBLE);
+                MengineSplashScreenPlugin.this.logMessage("hide splash screen");
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
+                MengineSplashScreenPlugin.this.logMessage("hided splash screen");
+
                 m_image.setVisibility(View.GONE);
 
-                MengineSplashScreenPlugin.this.removeSpash();
+                MengineSplashScreenPlugin.this.removeSpash(activity);
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
-                //Empty
             }
         });
 
-        m_image.startAnimation(animation);
+        m_image.clearAnimation();
+        m_image.startAnimation(hideAnimation);
     }
 
-    public void removeSpash() {
-        this.logMessage("removeSpash");
-
-        MengineActivity activity = this.getActivity();
-
+    private void removeSpash(@NonNull MengineActivity activity) {
         ViewGroup view = (ViewGroup)activity.getContentView();
         view.removeView(m_image);
+
         m_image = null;
     }
 }

c664dca13dddd478577e438436e6ce97998f926a
irov <irov13@mail.ru>
2023-03-06 19:21:47 +0200

    wip Apple plugin Sentry

---
+++ src/Plugins/AppleSentryPlugin/MacOSSentryApplicationDelegate.mm
@@ -0,0 +1,36 @@
+#import "AppleSentryApplicationDelegate.h"
+
+#import "Environment/Apple/AppleDetail.h"
+
+#include "Kernel/BuildMode.h"
+
+#import "Sentry/Sentry.h"
+
+@implementation AppleSentryApplicationDelegate
+
+#pragma mark - UIKitProxyApplicationDelegateInterface
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSString * AppleSentryPlugin_DSN = Mengine::Helper::AppleGetBundlePluginConfigString( @("MengineAppleSentryPlugin"), @("DSN"), nil );
+    
+    if( AppleSentryPlugin_DSN == nil )
+    {
+        return NO;
+    }
+
+    BOOL AppleSentryPlugin_Debug = Mengine::Helper::AppleGetBundlePluginConfigBoolean( @("MengineAppleSentryPlugin"), @("Debug"), NO );
+    
+    const Mengine::Char * BUILD_VERSION = Mengine::Helper::getBuildVersion();
+    
+    [SentrySDK startWithConfigureOptions:^(SentryOptions *options) {
+        options.dsn = AppleSentryPlugin_DSN;
+        options.debug = AppleSentryPlugin_Debug; // Enabled debug when first installing is always helpful
+        options.releaseName = @(BUILD_VERSION);
+        options.attachStacktrace = true;
+        options.tracesSampleRate = @(1.0);
+    }];
+    
+    return YES;
+}
+
+@end

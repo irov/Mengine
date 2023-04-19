97110f22521febd29a7b0146030b4a51c519085f
irov <irov13@mail.ru>
2023-04-19 21:14:26 +0300

    fix build msvc15

--- build/msvc15/make_solution_msvc15.bat
+++ build/msvc15/make_solution_msvc15.bat
@@ -6,13 +6,12 @@ set "YEAR=2017"
 set "SOLUTION_NAME=solution_msvc%VERSION%"
 set "SOURCE_DIRECTORY=%~dp0..\..\cmake\Win32"
 set "GENERATOR=Visual Studio %VERSION% %YEAR%"
-set "ARCHITECTURE=Win32"
 set "BUILD_PUBLISH=OFF"
 
 @call %~dp0../vcvarsall_msvc%VERSION%.bat
 
 @echo Starting make %SOLUTION_NAME% configuration...
 
-@call %~dp0../make_solution.bat %* "SOLUTION_NAME=%SOLUTION_NAME%" "SOURCE_DIRECTORY=%SOURCE_DIRECTORY%" "GENERATOR=%GENERATOR%" "BUILD_PUBLISH=%BUILD_PUBLISH%" "ARCHITECTURE=%ARCHITECTURE%"
+@call %~dp0../make_solution.bat %* "SOLUTION_NAME=%SOLUTION_NAME%" "SOURCE_DIRECTORY=%SOURCE_DIRECTORY%" "GENERATOR=%GENERATOR%" "BUILD_PUBLISH=%BUILD_PUBLISH%"
 
 @exit /b %errorlevel%

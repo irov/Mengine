8d876ed95032496b375773cf7ab1b6e4b7252455
irov <irov13@mail.ru>
2023-11-21 14:29:48 +0200

    fix AssertionCharacter

--- src/Kernel/CharacterHelper.h
+++ src/Kernel/CharacterHelper.h
@@ -6,6 +6,7 @@ namespace Mengine
 {
     namespace Helper
     {
+        bool isASCIICharacterSet( Char _ch );
         bool isStandardCharacterSet( Char _ch );
         bool isLowerCharacterSet( Char _ch );
         bool isUpperCharacterSet( Char _ch );

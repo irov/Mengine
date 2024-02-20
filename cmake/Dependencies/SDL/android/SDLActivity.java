3d8261c117a368a98ec19275a95fbc349642a5d5
irov <irov13@mail.ru>
2024-02-20 20:19:18 +0200

    fix android fullscreen

--- cmake/Dependencies/SDL/android/SDLActivity.java
+++ cmake/Dependencies/SDL/android/SDLActivity.java
@@ -418,7 +418,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
 
         setContentView(mLayout);
 
-        setWindowStyle(false);
+        setWindowStyle(true);
 
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
 

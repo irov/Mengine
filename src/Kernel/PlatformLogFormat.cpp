55d2cdc6d35b382dee19f12bf550277dadaa8c38
irov <irov13@mail.ru>
2024-02-26 20:10:35 +0200

    fix PlatformLogFormat

--- src/Kernel/PlatformLogFormat.cpp
+++ src/Kernel/PlatformLogFormat.cpp
@@ -1,14 +1,7 @@
 #include "PlatformLogFormat.h"
 
-#if defined(MENGINE_PLATFORM_APPLE)
-#   include "Environment/Apple/AppleUtils.h"
-#elif defined(MENGINE_PLATFORM_ANDROID)
-#   include <android/log.h>
-#else
-#   include "Config/StdIO.h"
-#endif
-
-#   include "Config/StdArg.h"
+#include "Config/StdIO.h"
+#include "Config/StdArg.h"
 
 namespace Mengine
 {
@@ -17,27 +10,16 @@ namespace Mengine
         //////////////////////////////////////////////////////////////////////////
         void PlatformLogMessage( const Char * _message )
         {
-#if defined(MENGINE_PLATFORM_APPLE)
-            Helper::AppleLogMessage( _message );
-#elif defined(MENGINE_PLATFORM_ANDROID)
-            __android_log_print( ANDROID_LOG_ERROR, "Mengine", "%s", _message );
-#else
-            ::printf( "%s", _message );
-#endif
+            std::printf( "%s", _message );
         }
         //////////////////////////////////////////////////////////////////////////
         void PlatformLogFormat( const Char * _format, ... )
         {
             MENGINE_VA_LIST_TYPE args;
-            MENGINE_VA_LIST_START( args, _format );            
-#if defined(MENGINE_PLATFORM_APPLE)
-            Helper::AppleLogFormatV( _format, args );
-#elif defined(MENGINE_PLATFORM_ANDROID)
-            __android_log_print( ANDROID_LOG_ERROR, "Mengine", _format, args );
-#else
-            ::vprintf( _format, args );
-#endif
+            MENGINE_VA_LIST_START( args, _format );
+            std::vprintf( _format, args );
             MENGINE_VA_LIST_END( args );
         }
+        //////////////////////////////////////////////////////////////////////////
     }
 }

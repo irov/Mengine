0c86acd370e0b3e41da262648419d32aaa98035d
irov <irov13@mail.ru>
2024-05-02 12:38:16 +0300

    clear files

--- src/SDLApplication/MengineAppleApplicationDelegates.mm
+++
@@ -1,7 +0,0 @@
-#import "MengineAppleApplicationDelegates.h"
-
-NSArray * getMengineAppleApplicationDelegates() {
-    NSArray * proxysClassed = [[NSArray alloc] initWithObjects:@"AppleGeneralDataProtectionRegulationApplicationDelegate", @"AppleAppLovinApplicationDelegate", @"AppleStoreInAppPurchaseApplicationDelegate", @"AppleFirebaseApplicationDelegate", @"AppleFirebaseAnalyticsApplicationDelegate",  nil];
-    
-    return proxysClassed;
-}

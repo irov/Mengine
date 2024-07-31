4df85b3d8fdf9b727aa0a7599f041a74ead79f21
irov <irov13@mail.ru>
2024-07-31 23:48:06 +0300

    fix apple NSConstString

--- src/Environment/Apple/AppleKernelService.mm
+++ src/Environment/Apple/AppleKernelService.mm
@@ -33,46 +33,7 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     void AppleKernelService::_finalizeService()
     {
-        for( IntrusiveListConstStringHolderNSString::iterator
-            it = m_holdersNSString.begin();
-            it != m_holdersNSString.end(); )
-        {
-            IntrusiveListConstStringHolderNSString::iterator it_erase = it;
-
-            ConstStringHolderNSString * holder = *it;
-            ++it;
-
-            m_holdersNSString.erase( it_erase );
-
-            m_poolNSString.destroyT( holder );
-        }
-
-        m_holdersNSString.clear();
-        m_poolNSString.clear();
-    }
-    //////////////////////////////////////////////////////////////////////////
-    void AppleKernelService::stringize( NSString * _value, ConstString * const _cstr )
-    {
-        if( [_value length] == 0 )
-        {
-            *_cstr = ConstString::none();
-
-            return;
-        }
-
-        ConstStringHolderNSString * holder = m_poolNSString.createT();
-
-        holder->setNSString( _value );
-
-        if( STRINGIZE_SERVICE()
-            ->stringizeExternal( holder, _cstr ) == false )
-        {
-            m_poolNSString.destroyT( holder );
-
-            return;
-        }
-
-        m_holdersNSString.push_back( holder );
+        //Empty
     }
     //////////////////////////////////////////////////////////////////////////    
 }
--- src/Interface/AppleKernelServiceInterface.h
+++ src/Interface/AppleKernelServiceInterface.h
@@ -2,23 +2,12 @@
 
 #include "Interface/ServiceInterface.h"
 
-#include "Kernel/ConstString.h"
-
-#if defined(__OBJC__)
-#import <Foundation/Foundation.h>
-#else
-class NSString;
-#endif
-
 namespace Mengine
 {
     class AppleKernelServiceInterface
         : public ServiceInterface
     {
         SERVICE_DECLARE( "AppleEnvironmentService" )
-        
-    public:
-        virtual void stringize( NSString * _value, ConstString * const _cstr ) = 0;
     };
 }
 //////////////////////////////////////////////////////////////////////////

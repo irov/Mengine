3ca45f96b951d3344e2b9ad3d55bb7a11aa31502
irov <irov13@mail.ru>
2024-08-04 23:23:58 +0300

    fix .circleci fix android HttpNetwork

--- gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineHttpResponseParam.java
+++ gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineHttpResponseParam.java
@@ -6,6 +6,5 @@ public class MengineHttpResponseParam {
     public int HTTP_RESPONSE_CODE;
     public int HTTP_CONTENT_LENGTH;
     public byte[] HTTP_CONTENT_DATA;
-    public String HTTP_CONTENT_JSON;
     public String HTTP_ERROR_MESSAGE;
 }

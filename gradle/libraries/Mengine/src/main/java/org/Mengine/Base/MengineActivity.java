98aeda944da6d99250f8a723f8665c0df68bed28
irov <irov13@mail.ru>
2024-08-15 17:35:17 +0300

    implement android show keyboard fix OpenAL sound system

--- gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineActivity.java
+++ gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineActivity.java
@@ -17,6 +17,7 @@ import android.view.Window;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;

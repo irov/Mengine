2d64455993ec29be52c42b585ebb899aeebb7ddb
irov <irov13@mail.ru>
2025-03-09 15:23:15 +0200

    fix android MengineAdResponseInterface

--- gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineAdResponseInterface.java
+++ gradle/libraries/Mengine/src/main/java/org/Mengine/Base/MengineAdResponseInterface.java
@@ -3,8 +3,8 @@ package org.Mengine.Base;
 import androidx.annotation.NonNull;
 
 public interface MengineAdResponseInterface {
-    void onAdRevenuePaid(@NonNull MengineAdMediation mediation, @NonNull MengineAdFormat format, String placement, double revenue);
     void onAdShowSuccess(@NonNull MengineAdMediation mediation, @NonNull MengineAdFormat format, String placement);
     void onAdShowFailed(@NonNull MengineAdMediation mediation, @NonNull MengineAdFormat format, String placement, int errorCode);
+    void onAdRevenuePaid(@NonNull MengineAdMediation mediation, @NonNull MengineAdFormat format, String placement, double revenue);
     void onAdUserRewarded(@NonNull MengineAdMediation mediation, @NonNull MengineAdFormat format, String placement, String label, int amount);
 }

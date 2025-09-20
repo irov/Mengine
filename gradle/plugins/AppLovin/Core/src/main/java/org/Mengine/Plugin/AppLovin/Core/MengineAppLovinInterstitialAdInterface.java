9f48fb7660243d5b7b1b86f4ee9be7375bc6be02
irov <irov13@mail.ru>
2025-09-21 01:38:22 +0300

    ref-in android AdService isShowingInterstitial&isShowingRewarded

--- gradle/plugins/AppLovin/Core/src/main/java/org/Mengine/Plugin/AppLovin/Core/MengineAppLovinInterstitialAdInterface.java
+++ gradle/plugins/AppLovin/Core/src/main/java/org/Mengine/Plugin/AppLovin/Core/MengineAppLovinInterstitialAdInterface.java
@@ -6,6 +6,6 @@ import org.Mengine.Base.MengineActivity;
 
 public interface MengineAppLovinInterstitialAdInterface extends MengineAppLovinAdInterface {
     boolean canYouShowInterstitial(String placement);
-
     boolean showInterstitial(@NonNull MengineActivity activity, String placement);
+    boolean isShowingInterstitial();
 }

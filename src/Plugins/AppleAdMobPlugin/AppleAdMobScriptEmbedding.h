71baf137145c376849862fd6019584c8932bdeb4
irov <irov13@mail.ru>
2025-11-17 13:29:25 +0200

    fix

---
+++ src/Plugins/AppleAdMobPlugin/AppleAdMobScriptEmbedding.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Interface/ScriptEmbeddingInterface.h"
+
+#include "Kernel/Factorable.h"
+
+namespace Mengine
+{
+    class AppleAdMobScriptEmbedding
+        : public ScriptEmbeddingInterface
+        , public Factorable
+    {
+    public:
+        AppleAdMobScriptEmbedding();
+        ~AppleAdMobScriptEmbedding() override;
+
+    public:
+        bool embed( pybind::kernel_interface * _kernel ) override;
+        void eject( pybind::kernel_interface * _kernel ) override;
+    };
+}
+

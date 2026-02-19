eca97962ff79de42a131550ea9bc1c5f2d1623bd
irov <irov13@mail.ru>
2026-02-19 19:19:31 +0200

    wip ios

--- src/Plugins/AppleAdMobPlugin/AppleAdMobPlugin.h
+++ src/Plugins/AppleAdMobPlugin/AppleAdMobPlugin.h
@@ -2,19 +2,13 @@
 
 #include "Kernel/PluginBase.h"
 
-#if defined(MENGINE_BUILD_MENGINE_SCRIPT_EMBEDDED)
-#   include "AppleAdMobScriptEmbedding.h"
-#endif
-
 namespace Mengine
 {    
     class AppleAdMobPlugin
         : public PluginBase
-        , protected EXTEND_EMBEDDABLE( AppleAdMobScriptEmbedding )
     {
         PLUGIN_DECLARE( "AppleAdMob" );
-        DECLARE_EMBEDDABLE();
-
+        
     public:
         AppleAdMobPlugin();
         ~AppleAdMobPlugin() override;

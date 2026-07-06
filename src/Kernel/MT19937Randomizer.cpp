9386453a6776c6413dc4b1c7f28e6a02cf2491dd
yurii.levchenko <yurii.levchenko@Yuriis-Mac-mini.local>
2026-07-06 20:11:59 +0200

    Fix randomizer range validation

--- src/Kernel/MT19937Randomizer.cpp
+++ src/Kernel/MT19937Randomizer.cpp
@@ -4,6 +4,8 @@
 #include "Kernel/Assertion.h"
 #include "Kernel/Logger.h"
 
+#include "Config/StdIntTypes.h"
+
 namespace Mengine
 {
     //////////////////////////////////////////////////////////////////////////
@@ -52,14 +54,14 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     uint32_t MT19937Randomizer::getRandomRange32( uint32_t _min, uint32_t _max ) const
     {
-        if( _max < 2 )
-        {
-            return MENGINE_UINT32_C(0);
-        }
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %" MENGINE_PRIu32 " max %" MENGINE_PRIu32
+            , _min
+            , _max
+        );
 
         if( _min >= _max )
         {
-            return _max - 1;
+            return _min;
         }
 
         StdRandom::uniform_int_distribution<uint32_t> uid( _min, _max - 1 );
@@ -80,9 +82,14 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     int32_t MT19937Randomizer::getRandomRange32i( int32_t _min, int32_t _max ) const
     {
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %" MENGINE_PRId32 " max %" MENGINE_PRId32
+            , _min
+            , _max
+        );
+
         if( _min >= _max )
         {
-            return _max - 1;
+            return _min;
         }
 
         StdRandom::uniform_int_distribution<int32_t> uid( _min, _max - 1 );
@@ -108,14 +115,14 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     uint64_t MT19937Randomizer::getRandomRange64( uint64_t _min, uint64_t _max ) const
     {
-        if( _max < 2 )
-        {
-            return MENGINE_UINT32_C(0);
-        }
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %" MENGINE_PRIu64 " max %" MENGINE_PRIu64
+            , _min
+            , _max
+        );
 
         if( _min >= _max )
         {
-            return _max - 1;
+            return _min;
         }
 
         StdRandom::uniform_int_distribution<uint64_t> uid( _min, _max - 1 );
@@ -136,9 +143,14 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     int64_t MT19937Randomizer::getRandomRange64i( int64_t _min, int64_t _max ) const
     {
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %" MENGINE_PRId64 " max %" MENGINE_PRId64
+            , _min
+            , _max
+        );
+
         if( _min >= _max )
         {
-            return _max - 1;
+            return _min;
         }
 
         StdRandom::uniform_int_distribution<int64_t> uid( _min, _max - 1 );
@@ -168,22 +180,21 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     float MT19937Randomizer::getRandomRangef( float _min, float _max ) const
     {
-        MENGINE_ASSERTION_FATAL( _min >= 0.f, "invalid min value %f"
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %f max %f"
             , _min
-        );
-
-        MENGINE_ASSERTION_FATAL( _max >= 0.f, "invalid max value %f"
             , _max
         );
 
-        if( _max < m_epsilonf )
-        {
-            return 0.f;
-        }
-
         if( _min >= _max )
         {
-            return _max;
+            return _min;
+        }
+
+        float range = _max - _min;
+
+        if( range < m_epsilonf )
+        {
+            return _min;
         }
 
         StdRandom::uniform_real_distribution<float> uid( _min, _max );
@@ -213,22 +224,21 @@ namespace Mengine
     //////////////////////////////////////////////////////////////////////////
     double MT19937Randomizer::getRandomRanged( double _min, double _max ) const
     {
-        MENGINE_ASSERTION_FATAL( _min >= 0.0, "invalid min value %lf"
+        MENGINE_ASSERTION_FATAL( _min <= _max, "invalid range min %lf max %lf"
             , _min
-        );
-
-        MENGINE_ASSERTION_FATAL( _max >= 0.0, "invalid max value %lf"
             , _max
         );
 
-        if( _max < m_epsilond )
-        {
-            return 0.0;
-        }
-
         if( _min >= _max )
         {
-            return _max;
+            return _min;
+        }
+
+        double range = _max - _min;
+
+        if( range < m_epsilond )
+        {
+            return _min;
         }
 
         StdRandom::uniform_real_distribution<double> uid( _min, _max );
